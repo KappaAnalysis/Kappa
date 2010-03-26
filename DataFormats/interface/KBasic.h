17e7acdb287a6a15f1bc1ea3c5b849e9c9a280ed
Manuel Zeise <zeise@cern.ch>
2010-03-26 23:26:06 +0100

    added function for impact parameter to KDataTrack

--- DataFormats/interface/KBasic.h
+++ DataFormats/interface/KBasic.h
@@ -12,16 +12,6 @@ struct KDataLV
 };
 typedef std::vector<KDataLV> KDataLVs;
 
-struct KDataTrack : public KDataLV
-{
-	RMPoint ref;	// reference point ("vertex")
-	char charge;
-	float chi2, nDOF;
-	float errPt, errEta, errPhi, errDxy;
-	int quality;
-};
-typedef std::vector<KDataTrack> KDataTracks;
-
 struct KDataVertex
 {
   RMPoint position;
@@ -33,6 +23,58 @@ struct KDataVertex
 	ROOT::Math::SMatrix<double, 3, 3, ROOT::Math::MatRepSym<double, 3> > covariance;
 };
 typedef std::vector<KDataVertex> KDataVertices;
+
+struct KDataTrack : public KDataLV
+{
+	RMPoint ref;	// reference point ("vertex")
+	char charge;
+	float chi2, nDOF;
+	float errPt, errEta, errPhi, errDxy;
+	int quality;
+
+	double getDxy(KDataVertex * pv)
+	{
+		if (!pv)
+			return -1.;
+		return (
+				- (ref.x()-pv->position.x()) * p4.y()
+				+ (ref.y()-pv->position.y()) * p4.x()
+			) / sqrt(p4.Perp2());
+	}
+
+	/*
+		mode:
+			0 - dxy
+			1 - dxy/error(track)
+			2 - dxy/sqrt(error(track)**2 + error(vertex)**2)
+	*/
+	double getIP(KDataVertex * pv, unsigned int mode = 0)
+	{
+		if (!pv)
+			return -10000.;
+
+		switch(mode)
+		{
+			case 0:
+				return getDxy(pv);
+				break;
+			case 1:
+				return getDxy(pv) / errDxy;
+				break;
+			case 2:
+				ROOT::Math::SVector<double,3> orthog;
+				orthog[0] = p4.y();
+				orthog[1] = -p4.x();
+				orthog[2] = 0;
+
+				float vtxErr2 = ROOT::Math::Similarity(pv->covariance, orthog) / p4.Perp2();
+				return getDxy(pv) / sqrt(errDxy*errDxy + vtxErr2 );
+				break;
+		}
+		return -10000.;
+	}
+};
+typedef std::vector<KDataTrack> KDataTracks;
 
 struct KDataJet : public KDataLV
 {
