189cde9c7df6113a3d49d1fb7e7a671756c56352
Fred Stober <stober@cern.ch>
2010-03-27 13:17:42 +0100

    MuonRef implemented

--- Producers/interface/KMuonProducer.h
+++ Producers/interface/KMuonProducer.h
@@ -2,7 +2,9 @@
 #define KAPPA_MUONPRODUCER_H
 
 #include "KBaseMultiLVProducer.h"
+#include "KTrackProducer.h"
 #include <DataFormats/MuonReco/interface/Muon.h>
+#include <DataFormats/MuonReco/interface/MuonFwd.h>
 //#include <DataFormats/TrackReco/interface/Track.h>
 #include <DataFormats/MuonReco/src/MuonSelectors.cc>
 #include <bitset>
@@ -26,9 +28,12 @@ public:
 		out.p4.SetCoordinates(in.pt(), in.eta(), in.phi(), 0);
 
 		// Tracks
-		out.track=getMTATrack(in.track());
-		out.globalTrack=getMTATrack(in.globalTrack());
-		out.innerTrack=getMTATrack(in.innerTrack());
+		if (in.track().isNonnull())
+			KTrackProducer::fillTrack(*in.track(), out.track);
+		if (in.globalTrack().isNonnull())
+			KTrackProducer::fillTrack(*in.globalTrack(), out.globalTrack);
+		if (in.innerTrack().isNonnull())
+			KTrackProducer::fillTrack(*in.innerTrack(), out.innerTrack);
 
 		// Charge, ...
 		out.charge = in.charge();
@@ -36,9 +41,9 @@ public:
 		out.caloComp = in.caloCompatibility();
 		out.segComp = muon::segmentCompatibility(in);
 		if (!in.track())
-			out.trackHits				= -1;
+			out.trackHits = -1;
 		else
-			out.trackHits				= in.track()->recHitsSize();
+			out.trackHits = in.track()->recHitsSize();
 
 		out.type = 8*in.isGlobalMuon()+4*in.isStandAloneMuon()+2*in.isCaloMuon()+in.isTrackerMuon();
 		out.isTrackerMuon			= in.isTrackerMuon();
@@ -54,21 +59,21 @@ public:
 		out.isGoodMuon = (unsigned int)tmpBits.to_ulong();
 
 		// Isolation
-		// FIXME: wie komme ich an die muonref ran?
-		// edm::RefToBase<reco::Muon> muonref(muons, it-muons->begin());
-		//reco::IsoDeposit muonIsoDeposit = (*isoDeps)[muonref];
+		// FIXME: isodeposit header
+		edm::RefToBase<reco::Muon> muonref(edm::Ref<edm::View<reco::Muon> >(handle, this->nCursor));
+//		reco::IsoDeposit muonIsoDeposit = (*isoDeps)[muonref];
 
 		out.ecalIso03					= in.isolationR03().emEt;
 		out.hcalIso03					= in.isolationR03().hadEt;
-		//out.trackIso03=muonIsoDeposit.depositWithin(0.3, isoParams, false);
+//		out.trackIso03					= muonIsoDeposit.depositWithin(0.3, isoParams, false);
 
 		out.ecalIso05					= in.isolationR05().emEt;
 		out.hcalIso05					= in.isolationR05().hadEt;
-		//out.trackIso05=muonIsoDeposit.depositWithin(0.5, isoParams, false);
+//		out.trackIso05					= muonIsoDeposit.depositWithin(0.5, isoParams, false);
 
 		//out.hcalIso06;
 		//out.ecalIso06;
-		//out.trackIso06=muonIsoDeposit.depositWithin(0.6, isoParams, false);
+//		out.trackIso06					= muonIsoDeposit.depositWithin(0.6, isoParams, false);
 
 		// Vertex
 		if (in.vertex().x()*in.vertex().x()+in.vertex().y()*in.vertex().y()+in.vertex().z()*in.vertex().z() != 0)
@@ -105,26 +110,6 @@ public:
 		return 1;				// InsideOut
 	}
 
-	KDataTrack getMTATrack(reco::TrackRef trk)
-	{
-		KDataTrack tmpTrk;
-		if (trk.isNonnull())
-			return tmpTrk;
-
-		tmpTrk.p4.SetCoordinates(trk->pt(), trk->eta(), trk->phi(), 0);
-		tmpTrk.charge = trk->charge();
-		tmpTrk.chi2 = trk->chi2();
-		tmpTrk.nDOF = trk->ndof();
-
-		tmpTrk.errPt = trk->ptError();
-		tmpTrk.errEta = trk->etaError();
-		tmpTrk.errPhi = trk->phiError();
-
-		tmpTrk.ref = trk->referencePoint();
-		tmpTrk.quality=trk->qualityMask();
-
-		return tmpTrk;
-	}
 };
 
 #endif
