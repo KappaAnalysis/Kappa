c55533a8b84974080bc4964c2954ae678a099796
Danilo Piparo <danilo.piparo@cern.ch>
2010-03-30 13:20:16 +0200

    o Change of TFile service location

--- Producers/src/KTuple.cc
+++ Producers/src/KTuple.cc
@@ -7,7 +7,7 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
 
 #include "FWCore/ServiceRegistry/interface/Service.h"
-#include "PhysicsTools/UtilAlgos/interface/TFileService.h"
+#include "CommonTools/UtilAlgos/interface/TFileService.h"
 
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include <TFile.h>
