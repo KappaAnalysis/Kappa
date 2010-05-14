3ccac8290594271a09a785fe64be69b892406c0a
Fred Stober <stober@cern.ch>
2010-05-14 02:00:00 +0200

    Added main entry point to include Kappa (autogenerated)

---
+++ DataFormats/interface/Kappa.h
@@ -0,0 +1,54 @@
+#ifndef KDATAFORMAT_H
+#define KDATAFORMAT_H
+
+#include "KBasic.h"
+#include "KLorentzVector.h"
+#include "KMetadata.h"
+#include "KMuon.h"
+#include "KTau.h"
+#include "KTrack.h"
+
+template<typename T>
+struct TypeName;
+
+#define REGISTER_NAME_OF_TYPE(X)  \
+	template<> struct TypeName<X> \
+	{                             \
+		static const char *name;  \
+	};                            \
+	const char *TypeName<X>::name = #X
+
+REGISTER_NAME_OF_TYPE(KDataLV);
+REGISTER_NAME_OF_TYPE(KDataLVs);
+REGISTER_NAME_OF_TYPE(KLV);
+REGISTER_NAME_OF_TYPE(KLVs);
+REGISTER_NAME_OF_TYPE(KDataMET);
+REGISTER_NAME_OF_TYPE(KDataPFMET);
+REGISTER_NAME_OF_TYPE(KDataJet);
+REGISTER_NAME_OF_TYPE(KDataJets);
+REGISTER_NAME_OF_TYPE(KDataPFJet);
+REGISTER_NAME_OF_TYPE(KDataPFJets);
+REGISTER_NAME_OF_TYPE(KDataTrack);
+REGISTER_NAME_OF_TYPE(KDataTracks);
+REGISTER_NAME_OF_TYPE(KTrackSummary);
+REGISTER_NAME_OF_TYPE(KDataVertex);
+REGISTER_NAME_OF_TYPE(KDataVertices);
+REGISTER_NAME_OF_TYPE(KParton);
+REGISTER_NAME_OF_TYPE(KPartons);
+REGISTER_NAME_OF_TYPE(KDataMuon);
+REGISTER_NAME_OF_TYPE(KDataMuons);
+REGISTER_NAME_OF_TYPE(KDataTau);
+REGISTER_NAME_OF_TYPE(KDataTaus);
+REGISTER_NAME_OF_TYPE(KDataCaloTau);
+REGISTER_NAME_OF_TYPE(KDataCaloTaus);
+REGISTER_NAME_OF_TYPE(KDataPFTau);
+REGISTER_NAME_OF_TYPE(KDataPFTaus);
+REGISTER_NAME_OF_TYPE(KDataGenTau);
+REGISTER_NAME_OF_TYPE(KDataGenTaus);
+REGISTER_NAME_OF_TYPE(KProvenance);
+REGISTER_NAME_OF_TYPE(KLumiMetadata);
+REGISTER_NAME_OF_TYPE(KGenLumiMetadata);
+REGISTER_NAME_OF_TYPE(KEventMetadata);
+REGISTER_NAME_OF_TYPE(KGenEventMetadata);
+
+#endif
