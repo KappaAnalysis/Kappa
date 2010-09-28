8c99fc324568bc182436ca492efd2de157382342
Armin Burgmeier <burgmeier@ekp.uni-karlsruhe.de>
2010-09-28 15:31:52 +0200

    Don't continue if duplicate producers are detected

--- Producers/src/KTuple.cc
+++ Producers/src/KTuple.cc
@@ -89,8 +89,25 @@ KTuple::KTuple(const edm::ParameterSet &psConfig)
 
 	KBaseProducer::verbosity = std::max(KBaseProducer::verbosity, psConfig.getParameter<int>("verbose"));
 
-	// Create metadata producer
 	std::vector<std::string> active = psConfig.getParameter<std::vector<std::string> >("active");
+
+	// Make sure there are no duplicates
+	bool have_duplicates = false;
+	for(size_t i = 0; i < active.size(); ++i)
+	{
+		for(size_t j = i+1; j < active.size(); ++j)
+		{
+			if(active[i] == active[j])
+			{
+				std::cout << "Duplicate producer found: " << active[i] << std::endl;
+				have_duplicates = true;
+			}
+		}
+	}
+
+	if(have_duplicates) exit(1);
+
+	// Create metadata producer
 	for (size_t i = 0; i < active.size(); ++i)
 	{
 		std::cout << "Init producer " << active[i] << std::endl;
