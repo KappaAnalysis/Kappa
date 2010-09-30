30f10d6669595f87e5b35a3f56103384087fcae1
Fred Stober <stober@cern.ch>
2010-09-30 15:32:23 +0200

    tiny config change

--- Skimming/skim_MC_GEN.py
+++ Skimming/skim_MC_GEN.py
@@ -1,31 +1,31 @@
-filenames=['file:///tmp/tst.root']
-
-# General parameters -----------------------------------------------------------
 import FWCore.ParameterSet.Config as cms
+
+# Basic process setup ----------------------------------------------------------
 process = cms.Process("kappaSkim")
-process.load('Configuration/StandardSequences/Services_cff')
 process.load("FWCore.MessageService.MessageLogger_cfi")
-process.load("Kappa.Producers.KTuple_cff")
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
-process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(filenames))
+process.load('Configuration/StandardSequences/Services_cff')
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
+	'file:///work/ws/bd105/CMSSW_Samples/PtJA/GEN-SIM-RECO/900_V8K_Z1_c85522a9-b01f-497d-b020-081566831f65.root',
+))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 #-------------------------------------------------------------------------------
 
 # Configure tuple generation ---------------------------------------------------
+process.load("Kappa.Producers.KTuple_cff")
 process.kappatuple = cms.EDAnalyzer('KTuple',
 	process.kappaTupleDefaultsBlock,
 	outputFile = cms.string('skim.root'),
 )
 process.kappatuple.verbose = cms.int32(10)
-process.kappatuple.active = cms.vstring('GenMetadata', 'Hits')
+process.kappatuple.active = cms.vstring('GenMetadata', 'LV')
 process.kappatuple.Metadata.l1Source = cms.InputTag("")
 process.kappatuple.Metadata.hltSource = cms.InputTag("")
 process.kappatuple.Metadata.noiseHCAL = cms.InputTag("")
 process.kappatuple.Metadata.hlTrigger = cms.InputTag("")
 process.kappatuple.Metadata.muonTriggerObjects = cms.vstring()
-process.kappatuple.Metadata.noiseHCAL = cms.InputTag("")
+#-------------------------------------------------------------------------------
 
+# Process schedule -------------------------------------------------------------
 process.pathDAT = cms.Path(process.kappatuple)
-
-# The schedule -----------------------------------------------------------------
 process.schedule = cms.Schedule(process.pathDAT)
 #-------------------------------------------------------------------------------
