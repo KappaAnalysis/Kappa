f64c4ee41b8bc8ed6297b592c2208151928d8a85
Fred Stober <stober@cern.ch>
2010-10-18 15:42:41 +0200

    fall2010 nickname producer

--- Skimming/kappaNick.py
+++ Skimming/kappaNick.py
@@ -9,13 +9,13 @@ def lookup(data, keyMap, default = None):
 	return (default, '')[default == None]
 
 def addPart(data):
-	return ("_%s" % data, '')[data == '']
+	return ('_%s' % data, '')[data == '']
 
 def parseCuts(data):
 	tmp = re.findall('([0-9]+)to([0-9]+)', data.lower())
 	if tmp:
 		return tmp[0]
-	return re.findall('pt[-_]*([0-9]+)', data.lower())
+	return filter(lambda x: int(x) > 0, re.findall('pt[-_]*([0-9]+)', data.lower()))
 
 
 class Summer10Nicks(NickNameProducer):
@@ -56,16 +56,41 @@ class Summer10Nicks(NickNameProducer):
 			'6Jets': '6j',
 		}
 
-		nick = "%s%s%s" % (lookup(pd, dsMap, 'xyz'), lookup(pd, genMap, 'P6'), lookup(pd, metaMap, ''))
+		nick = '%s%s%s' % (lookup(pd, dsMap, 'xyz'), lookup(pd, genMap, 'P6'), lookup(pd, metaMap, ''))
 		nick += addPart(lookup(pd, tuneMap, 'D6T'))
 		nick += addPart(lookup(sd, condMap))
 		if 'flat' in pd.lower():
-			nick += "_flat"
+			nick += '_flat'
 		else:
-			nick += addPart(str.join('_', map(lambda x: "%04d" % int(x), parseCuts(pd))))
+			nick += addPart(str.join('_', map(lambda x: '%04d' % int(x), parseCuts(pd))))
 		return nick
 
 
 class Fall10Nicks(NickNameProducer):
 	def getName(self, oldnick, dataset):
-		return oldnick
+		if oldnick != '':
+			return oldnick
+		(pd, sd, tier) = dataset.lstrip('/').split('/')
+
+		dsMap = {
+			'QCD': 'qcd',
+			'MinBias': 'mb',
+		}
+		genMap = {
+			'pythia8': 'P8',
+		}
+		condMap = {
+			'Fall10-START38_V12': 'F10S38v12',
+		}
+		tuneMap = dict(map(lambda x: ('Tune' + x, x), ['Z2', 'ProPT0', 'P0', 'DW', 'D6T', 'CW']))
+		tuneMap['pythia8'] = 'T1'
+		metaMap = dict(map(lambda x: (x, x), ['TauBiased', 'MuEnrichedPt', 'MuPt5Enriched', 'EMEnriched']))
+		nick = '%s%s' % (lookup(pd, dsMap, 'xyz'), lookup(pd, genMap, 'P6'))
+		nick += addPart(lookup(pd, tuneMap, 'D6T'))
+		nick += addPart(lookup(sd, condMap))
+		nick += addPart(lookup(pd, metaMap))
+		if 'flat' in pd.lower():
+			nick += '_flat'
+		else:
+			nick += addPart(str.join('_', map(lambda x: '%04d' % int(x), parseCuts(pd))))
+		return nick
