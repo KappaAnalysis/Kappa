5c3bcb48043877856c7773f167765126dbc37cf9
Fred Stober <stober@cern.ch>
2011-06-08 16:26:04 +0200

    Added old skimming file

---
+++ Skimming/skim_C11_41x_jet.py
@@ -0,0 +1,78 @@
+import FWCore.ParameterSet.Config as cms
+from FWCore.ParameterSet.VarParsing import VarParsing
+
+options = VarParsing ('analysis')
+options.parseArguments()
+
+# Basic process setup ----------------------------------------------------------
+process = cms.Process("kappaSkim")
+process.source = cms.Source("PoolSource", fileNames = cms.untracked.vstring(
+	'file://kostas_slimming_test.root',
+))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+#-------------------------------------------------------------------------------
+
+# Includes + Global Tag --------------------------------------------------------
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load('Configuration/StandardSequences/Services_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Geometry_cff')
+process.load('Configuration/StandardSequences/GeometryPilot2_cff')
+#process.load('RecoJets.Configuration.RecoJetAssociations_cff')
+process.load('Configuration/StandardSequences/FrontierConditions_GlobalTag_cff')
+process.load("Configuration.StandardSequences.Reconstruction_cff")
+process.GlobalTag.globaltag = 'GR_P_V14::All'
+#-------------------------------------------------------------------------------
+
+# Produce rho distribution------------------------------------------------------
+process.load('RecoJets.JetProducers.kt4PFJets_cfi')
+process.kt6PFJetsRho = process.kt4PFJets.clone( rParam = 0.6, doRhoFastjet = True )
+process.kt6PFJetsRho.Rho_EtaMax = cms.double(5.)
+process.JetArea = cms.Path(process.kt6PFJetsRho)
+#-------------------------------------------------------------------------------
+
+# Configure tuple generation ---------------------------------------------------
+process.load("Kappa.Producers.KTuple_cff")
+process.kappatuple = cms.EDAnalyzer('KTuple',
+	process.kappaTupleDefaultsBlock,
+	outputFile = cms.string('skim.root'),
+	CaloJets = cms.PSet(
+		process.kappaNoCut,
+		process.kappaNoRegEx,
+		# only with RECO: srcNoiseHCAL = cms.InputTag("hcalnoise"),
+		srcNoiseHCAL = cms.InputTag(""),
+		AK5CaloJets = cms.PSet(
+			src = cms.InputTag("ak5CaloJets"),
+			srcJetID = cms.InputTag("ak5JetID"),
+		),
+		AK7CaloJets = cms.PSet(
+			src = cms.InputTag("ak7CaloJets"),
+			srcJetID = cms.InputTag("ak7JetID"),
+		),
+		KT4CaloJets = cms.PSet(
+			src = cms.InputTag("kt4CaloJets"),
+			srcJetID = cms.InputTag("kt4JetID")
+		),
+		KT6CaloJets = cms.PSet(
+			src = cms.InputTag("kt6CaloJets"),
+			srcJetID = cms.InputTag("kt6JetID")
+		),
+	)
+)
+
+process.kappatuple.verbose = cms.int32(20)
+process.kappatuple.active = cms.vstring(
+	'Metadata', 'TrackSummary', 'VertexSummary', 'CaloJets', 'PFJets', 'JetArea'
+)
+process.kappatuple.Metadata.noiseHCAL = cms.InputTag("")
+
+#-------------------------------------------------------------------------------
+
+# Process schedule -------------------------------------------------------------
+#process.pathDAT = cms.Path(process.recoJetAssociations+process.kappatuple)
+process.pathDAT = cms.Path(process.kappatuple)
+process.schedule = cms.Schedule(process.JetArea, process.pathDAT)
+#-------------------------------------------------------------------------------
+process.options = cms.untracked.PSet(
+	SkipEvent = cms.untracked.vstring('ProductNotFound')
+)
