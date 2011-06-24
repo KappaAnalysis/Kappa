79e2d9d5e3307e50ffb49e3a7052cf9f141fae28
Fred Stober <stober@cern.ch>
2011-06-25 01:46:24 +0200

    wsn (cherry picked from commit 196c88eb689c79a592f3c32a810906a903626966)

--- DataFormats/interface/KBasic.h
+++ DataFormats/interface/KBasic.h
@@ -105,23 +105,24 @@ typedef std::vector<KPFCandidate> KPFCandidates;
 
 struct KL1Muon : KDataLV
 {
-  // bit    meaning
-  // 0-2    quality
-  // 3      isForward
-  // 4      isRPC
-  // 5      isMip
-  // 6      isIsolated
-  // 7-10   bx (15 = value of bx>=14)
-  // 11-13  detector
-  unsigned short state; // bit map
+	// bit    meaning
+	// 0-2    quality
+	// 3      isForward
+	// 4      isRPC
+	// 5      isMip
+	// 6      isIsolated
+	// 7-10   bx (15 = value of bx>=14)
+	// 11-13  detector
+	unsigned short state; // bit map
 
-  int quality() const { return (state & ( 7 )); };
-  bool isForward() const { return (state & (1 << 3)); };
-  bool isRPC() const { return (state & (1 << 4)); };
-  bool isMip() const { return (state & (1 << 5)); };
-  bool isIsolated() const { return (state & (1 << 6)); };
-  int bx() const { return ((state & (15 << 7)) >> 7) - 7; };
-  int detector() const { return ((state & (7 << 11)) >> 11);};
+	int quality() const { return (state & ( 7 )); };
+	bool isForward() const { return (state & (1 << 3)); };
+	bool isRPC() const { return (state & (1 << 4)); };
+	bool isMip() const { return (state & (1 << 5)); };
+	bool isIsolated() const { return (state & (1 << 6)); };
+	int bx() const { return ((state & (15 << 7)) >> 7) - 7; };
+	int detector() const { return ((state & (7 << 11)) >> 11);};
 };
 typedef std::vector<KL1Muon> KL1Muons;
+
 #endif
