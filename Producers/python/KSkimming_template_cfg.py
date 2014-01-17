1e8b31739c00d811f83fef3475a89c00bf4dd0c7
Roger <roger.wolf@cern.ch>
2014-01-17 13:38:50 +0100

    a new template with all necessary infomation to start a cmsRun job, process is called KAPPA, cms.EndPath is defined to run KTuple as defined in the Kappa/Producers/python/KTuple_cff.py

---
+++ Producers/python/KSkimming_template_cfg.py
@@ -0,0 +1,30 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("KAPPA")
+
+## MessageLogger
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+
+## Options and Output Report
+process.options = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
+
+## Source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring()
+)
+## Maximal Number of Events
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
+
+## Geometry and Detector Conditions (needed for a few patTuple production steps)
+process.load("Configuration.Geometry.GeometryIdeal_cff")
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+
+from Configuration.AlCa.autoCond import autoCond
+process.GlobalTag.globaltag = cms.string( autoCond[ 'startup' ] )
+process.load("Configuration.StandardSequences.MagneticField_cff")
+
+## Kappa interface 
+process.load('Kappa.Producers.KTuple_cff')
+process.kappaTuple = cms.EDAnalyzer('KTuple', process.kappaTupleDefaultsBlock,
+                                    outputFile = cms.string("kappaTuple_template.root"))
+process.kappaOut   = cms.EndPath(process.kappaTuple)
