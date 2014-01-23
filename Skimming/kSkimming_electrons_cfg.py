ab50161545093d11676a7d0a3394ffd59287bd9e
rfriese <raphael.friese@cern.ch>
2014-01-23 14:02:48 +0100

    Include ParticleFlow Candidates to Electron skim

--- Skimming/kSkimming_electrons_cfg.py
+++ Skimming/kSkimming_electrons_cfg.py
@@ -40,8 +40,11 @@ process.kappaOut+=process.edmOut
 ## ------------------------------------------------------------------------
 ## KappaElectrons 
 process.load("Kappa.Producers.KElectrons_cff")
+process.load("Kappa.Producers.KPFCandidates_cff")
+process.kappaTuple.PFCandidates.blacklist = cms.vstring("particleFlowTmp")
 
 ## And let it run
 process.p = cms.Path(
-    process.makeKappaElectrons
-    )
+	process.makeKappaElectrons *
+	process.makeKappaPFCandidates
+	)
