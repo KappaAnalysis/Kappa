79eb09359280b9b8c0990109b3d4e96980838f2a
Thomas Müller <tmueller@ekp.uni-karlsruhe.de>
2014-01-30 14:40:00 +0100

    added minimal skimming code for MVA MET

---
+++ Producers/python/KMET_cff.py
@@ -0,0 +1,22 @@
+import FWCore.ParameterSet.Config as cms
+
+# https://github.com/violatingcp/Jets_Short/blob/master/RecoJets/JetAnalyzers/test/JetMetPlotsExample.py#L220
+
+#Pileup Reduced MET
+#Load the conditions (needed for the JEC applied inside the MVA Met)
+from Configuration.StandardSequences.FrontierConditions_GlobalTag_cff import *
+from JetMETCorrections.Configuration.JetCorrectionProducers_cff import *
+
+#Load the MVA Met
+from RecoMET.METPUSubtraction.mvaPFMET_leptons_cff import *
+from RecoMET.METPUSubtraction.noPileUpPFMET_cff import *
+
+#Specify the leptons
+#pfMEtMVA.srcLeptons = cms.VInputTag("isomuons")#,"isoelectrons","isotaus")
+
+#Type1 Corrections
+from JetMETCorrections.Type1MET.correctionTermsPfMetType1Type2_cff import *
+from JetMETCorrections.Type1MET.correctedMet_cff import *
+JetCorrection = "ak5PFL1FastL2L3"
+corrPfMetType1.jetCorrLabel = cms.string(JetCorrection)
+
---
+++ Skimming/kSkimming_met_cfg.py
@@ -0,0 +1,35 @@
+import os
+import sys
+sys.path.append(os.path.abspath(os.path.dirname(sys.argv[0])) + "/Kappa/")
+
+## ------------------------------------------------------------------------
+## import skeleton process KAPPA
+from Kappa.Producers.KSkimming_template_cfg import *
+
+## test file for EKP
+process.source.fileNames = cms.untracked.vstring('file:///storage/6/fcolombo/TTH_Inclusive_M-125_8TeV_pythia6_AODSIM_PU_S10_START53_V7A-v1_0009E1F7-FD09-E211-8955-001E673986B0.root')
+## test file for lxplus
+#process.source.fileNames = cms.untracked.vstring('')
+
+process.maxEvents.input = 100 ## number of events to be processed (-1 = all in file)
+process.kappaTuple.outputFile = 'kappaTuple_met.root' ## name of output file
+process.kappaTuple.active = cms.vstring('MET', 'PFMET')
+process.kappaTuple.verbose = cms.int32(0) ## verbosity level
+
+## ------------------------------------------------------------------------
+## declare edm OutputModule (expects a path 'p'), uncommented if wanted
+process.edmOut = cms.OutputModule("PoolOutputModule",
+		fileName=cms.untracked.string('patTuple_met.root'),
+		SelectEvents=cms.untracked.PSet(SelectEvents=cms.vstring('p')), # save only events passing the full path
+		outputCommands=cms.untracked.vstring('drop *', 'keep *_*_*_KAPPA') # save each edm object that has been produced by process KAPPA 
+)
+process.kappaOut+=process.edmOut
+
+## ------------------------------------------------------------------------
+## KappaJets
+process.load("Kappa.Producers.KMET_cff")
+
+## And let it run
+process.p = cms.Path(
+		process.pfMEtMVAsequence
+)
