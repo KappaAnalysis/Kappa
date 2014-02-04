9a537b19cf074d7629833a4dcd2b9299d16b9f27
Joram Berger <joram.berger@cern.ch>
2014-02-04 18:03:35 +0100

    Add tau-tau specific trigger selection
    This adds the whole list from
    https://github.com/cms-analysis/HiggsAnalysis-KITHiggsToTauTau/blob/master/data/triggerTables-2011-2012.txt
    
    To see which triggers are actually stored, please set printHltList to True.
    In a next step this could be further simplified if enough bits are free.
    The blacklist could also help.

--- Skimming/kSkimming.py
+++ Skimming/kSkimming.py
@@ -27,6 +27,33 @@ def getBaseConfig(globaltag, testfile="", maxevents=100, datatype='data'):
 		process.kappaTuple.active		+= cms.vstring('GenMetadata')		## produce Metadata for MC,
 		process.kappaTuple.active		+= cms.vstring('GenParticles')		## save GenParticles,
 
+	process.kappaTuple.Metadata.hltWhitelist = cms.vstring(					## HLT selection
+		# https://github.com/cms-analysis/HiggsAnalysis-KITHiggsToTauTau/blob/master/data/triggerTables-2011-2012.txt
+		# e
+		"^HLT_Ele[0-9]+_CaloIdVT_(CaloIsoT)?_TrkIdT_(TrkIsoT)?_v[0-9]+$",
+		"^HLT_Ele[0-9]+_WP[0-9]+_v[0-9]+$",
+		# m
+		"^HLT_(Iso)?Mu[0-9]+_(eta2p1)?_v[0-9]+$",
+		# ee
+		"^HLT_Ele[0-9]+_CaloId(L|T)(_TrkIdVL)?_CaloIsoVL(_TrkIdVL)?(_TrkIsoVL)?" +
+			"_Ele[0-9]+_CaloId(L|T)(_TrkIdVL)?_CaloIsoVL(_TrkIdVL)?(_TrkIsoVL)?_v[0-9]+$",
+		# mm
+		"^HLT_(Double)Mu[0-9]+(_(Mu|Jet)[0-9]+)?_v[0-9]+$",
+		# em
+		"^HLT_Mu[0-9]+_(Ele|Photon)[0-9]+_CaloId(L|T|VT)(_CaloIsoVL|_IsoT)?(_TrkIdVL_TrkIsoVL)?_v[0-9]+$",
+		# et
+		"^HLT_Ele[0-9]+_CaloIdVT(_Calo(IsoRho|Iso)T)?_TrkIdT(_TrkIsoT)?_(Loose|Medium|Tight)IsoPFTau[0-9]+_v[0-9]+$",
+		"^HLT_Ele[0-9]+_eta2p1_WP[0-9]+(Rho|NoIso)_LooseIsoPFTau[0-9]+_v[0-9]+$",
+		# mt
+		"^HLT_(Iso)?Mu[0-9]+(_eta2p1)?_(Loose|Medium|Tight)IsoPFTau[0-9]+(_Trk[0-9]_eta2p1)?_v[0-9]+$",
+		# tt
+		"^HLT_Double(Medium)?IsoPFTau[0-9]+_Trk[0-9]_eta2p1_(Jet[0-9]+|Prong[0-9])?_v[0-9]+$",
+		# specials (possible generalization: Mu15, L1ETM20, Photon20, Ele8)
+		"^HLT_Ele[0-9]+_CaloId(L|T|VT)_CaloIso(VL|T|VT)(_TrkIdT)?(_TrkIsoVT)?_(SC|Ele)[0-9](_Mass[0-9]+)?_v[0-9]+$",
+		"^HLT_Ele8_v[0-9]+$",
+		"^HLT_IsoMu15(_eta2p1)?_L1ETM20_v[0-9]+$",
+		"^HLT_Photon20_CaloIdVT_IsoT_Ele8_CaloIdL_CaloIsoVL_v[0-9]+$",
+	)
 
 	## ------------------------------------------------------------------------
 	# Configure Electrons
