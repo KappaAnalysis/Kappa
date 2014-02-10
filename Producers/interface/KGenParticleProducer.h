58fab20481252070f5e9e25baff884a18c468a58
Roger <roger.wolf@cern.ch>
2014-02-10 17:35:14 +0100

    extended KGenParticleProducer by list of daugter indices

--- Producers/interface/KGenParticleProducer.h
+++ Producers/interface/KGenParticleProducer.h
@@ -6,6 +6,9 @@
 #ifndef KAPPA_PARTONPRODUCER_H
 #define KAPPA_PARTONPRODUCER_H
 
+#include <iostream>
+#include <map>
+
 #include "KBaseMultiProducer.h"
 #include <DataFormats/HepMCCandidate/interface/GenParticle.h>
 #include <bitset>
@@ -19,9 +22,26 @@ public:
 
 
 protected:
+	/// interface for other derived classes like KGenTauProducer, which do not provide information for daughter particles; the list of daughters is filled with an empty  dummy vector in this case
 	virtual void fillSingle(const typename KBaseMultiLVProducer<edm::View<reco::Candidate>, TProduct>::SingleInputType &in, typename KBaseMultiLVProducer<edm::View<reco::Candidate>, TProduct>::SingleOutputType &out)
 	{
-		copyP4(in, out.p4);
+	  std::vector<reco::Candidate::index> dummy;
+	  fillSingle(in, out, dummy);
+	}
+
+	virtual void fillSingle(const typename KBaseMultiLVProducer<edm::View<reco::Candidate>, TProduct>::SingleInputType &in, typename KBaseMultiLVProducer<edm::View<reco::Candidate>, TProduct>::SingleOutputType &out, std::vector<reco::Candidate::index> &daughters)
+	{
+	        copyP4(in, out.p4);
+
+		if (KBaseProducer::verbosity > 3){
+		  std::cout << "index in out : " << index << std::endl;
+		  std::cout << "pdgId        : " << in.pdgId() << std::endl;
+		  std::cout << "N(d)         : " << in.numberOfDaughters() << " | " << daughters.size() << std::endl;
+		  for (unsigned int nd=0; nd<in.numberOfDaughters() && nd<daughters.size(); ++nd){
+		    std::cout << "   - " << nd << " pdgId(daughter): " << in.daughter(nd)->pdgId() << " | index: " << daughters.at(nd) << std::endl;
+		  }
+		  std::cout << std::endl << std::endl;
+		}
 
 		unsigned int id = (in.pdgId() < 0) ? -in.pdgId() : in.pdgId();
 		out.pdgid = id | ((in.status() % 4) << KGenParticleStatusPosition);
@@ -31,10 +51,12 @@ protected:
 
 		if (in.pdgId() != out.pdgId())
 			std::cout << "The pdgId is not skimmed correctly! "
-				<< "in=" << in.pdgId() << ", out=" << out.pdgId()
+				  << "in=" << in.pdgId() << ", out=" << out.pdgId()
 				<< std::endl << std::flush;
 		assert(in.pdgId() == out.pdgId());
+		
 	}
+
 	virtual bool acceptSingle(const typename KBaseMultiLVProducer<edm::View<reco::Candidate>, TProduct>::SingleInputType &in)
 	{
 		bool acceptStatus = false;
@@ -64,6 +86,12 @@ protected:
 		selectedParticles.assign(begin, end);
 	}
 
+protected:
+	/// index to determine the position of each genParticle in the output vector 
+	reco::Candidate::index index;
+	/// mapping memory address of each genParticle to the position in the output vector
+	std::map<const reco::Candidate*, reco::Candidate::index> daughterDict;
+
 private:
 	int selectedStatus; // bit map
 	std::vector<int> selectedParticles;
@@ -81,14 +109,45 @@ protected:
 	virtual void fillProduct(const InputType &in, OutputType &out,
 		const std::string &name, const edm::InputTag *tag, const edm::ParameterSet &pset)
 	{
-		// Retrieve additional input products
-		int status = pset.getParameter<int>("selectedStatus");
-		std::vector<int> particles = pset.getParameter<std::vector<int> >("selectedParticles");
+	  if (KBaseProducer::verbosity > 0)
+	    std::cout << in.size() << "KGenParticleProducer::fillProduct :  objects in collection " << name << std::endl;
+	  
+	  out.reserve(in.size());
+	  
+	  // retrieve additional input products
+	  int status = pset.getParameter<int>("selectedStatus");
+	  std::vector<int> particles = pset.getParameter<std::vector<int> >("selectedParticles");
+	  
+	  selectStatus(status);
+	  selectParticles(particles.begin(), particles.end());
 
-		selectStatus(status);
-		selectParticles(particles.begin(), particles.end());
-
-		KBasicGenParticleProducer<KGenParticles>::fillProduct(in, out, name, tag, pset);
+	  // fill daughterDict for each genParticle
+	  daughterDict.clear();
+	  typename InputType::const_iterator lvit;
+	  for (index=0, lvit = in.begin(); lvit < in.end(); ++lvit){
+	    if (acceptSingle(*lvit)){
+	      const reco::Candidate* lvptr = dynamic_cast <const reco::Candidate*> (&(*lvit));
+ 	      daughterDict[lvptr] = index++;
+	    }
+	  }
+	  	  
+	  for (index=0, lvit = in.begin(), index=0; lvit < in.end(); ++lvit){
+	    if (acceptSingle(*lvit)){
+	      // obtain list of daughter indices
+	      std::vector<reco::Candidate::index> daughters;
+	      for (reco::Candidate::index nd=0; nd<lvit->numberOfDaughters(); ++nd){
+		daughters.push_back(daughterDict.find(lvit->daughter(nd))->second);
+	      }	            
+	      out.push_back(SingleOutputType());
+	      fillSingle(*lvit, out.back(), daughters);
+	      ++index;
+	    }
+	  }
+	  if (maxN > 0)
+	    out.erase(out.begin() + std::min(out.size(), (size_t)maxN), out.end());
+	  
+	  if (KBaseProducer::verbosity > 1)
+	    std::cout << "KGenParticleProducer::fillProduct : \t" << "Number of accepted objects: " << out.size() << "\t";
 	}
 };
 
