f4e37de34268846bd3494b3d7ea59ec373543d91
rfriese <raphael.friese@cern.ch>
2014-05-15 13:56:51 +0200

    fixed GlobalTag

--- Skimming/kSkimming_cfg.py
+++ Skimming/kSkimming_cfg.py
@@ -3,7 +3,7 @@ import sys
 import FWCore.ParameterSet.Config as cms
 import Kappa.Skimming.datasetsHelper as datasetsHelper
 
-def getBaseConfig(globaltag= 'START53_V15A', testfile=cms.untracked.vstring(""), maxevents=100, nickname = 'SM_VBFHToTauTau_M_90_powheg_pythia_8TeV', kappaTag = 'Kappa_1_0_0'):
+def getBaseConfig(globaltag= 'START53_V15A::All', testfile=cms.untracked.vstring(""), maxevents=100, nickname = 'SM_VBFHToTauTau_M_90_powheg_pythia_8TeV', kappaTag = 'Kappa_1_0_0'):
 
 	from Kappa.Producers.KSkimming_template_cfg import process
 	process.source.fileNames      = testfile
