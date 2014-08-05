d6bffbc640f7f4f14699ba227cafe9b8e8f2081b
Thomas Mueller <tmuller@cern.ch>
2014-08-05 13:44:58 +0200

    KDebug: adjusted debug function to new trigger objects structure

--- DataFormats/test/KDebug.cpp
+++ DataFormats/test/KDebug.cpp
@@ -216,13 +216,9 @@ std::ostream &displayHLT(std::ostream &os, const KLumiMetadata &metaLumi, const 
 	return os;
 }
 
-std::ostream &displayTO(std::ostream &os, const size_t hltIdx, const KTriggerObjects *trgObj)
+std::ostream &displayTO(std::ostream &os, const size_t hltIdx, const size_t filterIdx, const KTriggerObjects *trgObj)
 {
-	os << std::endl << "L1L2: ";
-	for (size_t i = 0; i < trgObj->toIdxL1L2[hltIdx].size(); ++i)
-		os << trgObj->trgObjects[trgObj->toIdxL1L2[hltIdx][i]] << " ";
-	os << std::endl << "HLT: ";
-	for (size_t i = 0; i < trgObj->toIdxHLT[hltIdx].size(); ++i)
-		os << trgObj->trgObjects[trgObj->toIdxHLT[hltIdx][i]] << " ";
+	for (size_t i = 0; i < trgObj->toIdxFilter[hltIdx][filterIdx].size(); ++i)
+		os << trgObj->trgObjects[trgObj->toIdxFilter[hltIdx][filterIdx][i]] << " ";
 	return os << std::endl;
 }
