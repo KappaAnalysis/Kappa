f99bcffc94c342fd867b64eeb2a9567f72f6f229
Benjamin Treiber <benjamin.treiber@gmail.com>
2014-10-21 17:31:11 +0200

    New tauon producer with modified sequence for run2

---
+++ Producers/python/KTaus_run2_cff.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+## ------------------------------------------------------------------------
+##  rerun tau reconstruction sequence following POG recommendation:
+##  https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuidePFTauID#5_3_12_and_higher
+##  rerun recoTauClassicHPSSequence to get newest taus
+from RecoTauTag.Configuration.RecoPFTauTag_cff import *
+
+## ------------------------------------------------------------------------
+## Definition of sequences
+
+## run this to get latest greatest HPS taus
+makeKappaTaus = cms.Sequence(
+    recoTauCommonSequence * # to create needed Jets for 'old' run1 tau recipie that are missing in run2-root files
+    recoTauClassicHPSSequence
+    )
