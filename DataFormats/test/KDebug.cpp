88fd73d77fef6b9d179b6179c83d374b05935162
Joram Berger <joram.berger@cern.ch>
2015-01-27 20:41:45 +0100

    Improve debug output to standard out (part 2)

--- DataFormats/test/KDebug.cpp
+++ DataFormats/test/KDebug.cpp
@@ -10,6 +10,19 @@
 //-  * Thomas Hauth <Thomas.Hauth@cern.ch>
 //-  * Thomas Mueller <tmuller@cern.ch>
 
+/* Extension of ostream to understand Kappa objects
+ * This can be used in the analysis code as: cout << jet;
+ * Basic rules for the output:
+ *  - use minimal space (preferably one line)
+ *  - stay roughly below 80 characters per line
+ *  - don't show deprecated members
+ *  - group members logically
+ *  - avoid redundant information
+ *  - don't show the object name at the beginning
+ *  - don't put a new line at the end
+ *  - use static_cast whereever possible
+ */
+
 #include "../interface/KDebug.h"
 #include <bitset>
 
@@ -41,7 +54,7 @@ std::ostream &operator<<(std::ostream &os, const KMET &met)
 	os << "\tCHF=" << met.chargedHadronFraction << " NHF=" << met.neutralHadronFraction;
 	os << " HFHadF=" << met.hfHadronFraction << " HFEMF=" << met.hfEMFraction << std::endl;
 	os << "\teF=" << met.electronFraction << " photF=" << met.photonFraction;
-	os << " muF=" << met.muonFraction << std::endl;
+	os << " muF=" << met.muonFraction;
 	return os;
 }
 
@@ -70,8 +83,20 @@ std::ostream &operator<<(std::ostream &os, const KParticle &p)
 
 std::ostream &operator<<(std::ostream &os, const KGenParticle &p)
 {
-	return os << static_cast<const KParticle>(p)
-		<< " status=" << p.status() << " pdgid=" << p.pdgId() << " charge=" << p.charge();
+	os << static_cast<const KParticle>(p) << std::endl;
+	os << "\tdaughters(" << p.daughterIndices.size() << ")";
+	if (p.daughterIndices.size() > 0)
+		os << "=" << p.daughterIndices[0];
+	for (size_t i = 1; i < p.daughterIndices.size(); ++i)
+		os << p.daughterIndices[i] << ", ";
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const KGenPhoton &p)
+{
+	os << static_cast<const KLV>(p) << std::endl;
+	os << "\ttype=" << int(p.type) << " mother=" << p.mother;
+	return os;
 }
 
 std::ostream &operator<<(std::ostream &os, const KElectron &e)
@@ -79,7 +104,7 @@ std::ostream &operator<<(std::ostream &os, const KElectron &e)
 	os << static_cast<const KLepton>(e);
 	os << " track=" << e.trackIso << " ecal=" << e.ecalIso << " hcal=" << e.hcal1Iso << "+" << e.hcal2Iso << std::endl;
 	os << "\tH/EM=" << e.hadronicOverEm << " fbrem=" << e.fbrem << " Eecal=" << e.ecalEnergy << "+-" << e.ecalEnergyErr;
-	os << " DpTrack=" << e.trackMomentumErr << " Dp" << e.electronMomentumErr;
+	os << " DpTrack=" << e.trackMomentumErr << " Dp=" << e.electronMomentumErr;
 	os << " type=" << int(e.electronType) << " flags=" << std::bitset<8>(e.fiducialFlags) << std::endl;
 	os << "\tvtx: eta=" << e.dEtaIn << " phi=" << e.dPhiIn << " sigma=" << e.sigmaIetaIeta;
 	os << "; supercluster: pos=" << e.superclusterPosition;
@@ -110,12 +135,13 @@ std::ostream &operator<<(std::ostream &os, const KBasicJet &jet)
 
 std::ostream &operator<<(std::ostream &os, const KJet &jet)
 {
-	os << static_cast<const KBasicJet>(jet) << std::endl;
+	os << static_cast<const KBasicJet>(jet);
+	os << "\tIDs=" << std::bitset<8>(jet.binaryIds);
 	if (jet.tags.size() > 0)
-		os << "\ttags: ";
+		os << std::endl << "\ttags: ";
 	for (size_t i = 0; i < jet.tags.size(); ++i)
 		os << jet.tags[i] << ", ";
-	return os << "\tIDs: " << std::bitset<8>(jet.binaryIds);
+	return os;
 }
 
 std::ostream &operator<<(std::ostream &os, const KLepton &lep)
@@ -159,7 +185,7 @@ std::ostream &operator<<(std::ostream &os, const KGenTau &tau)
 {
 	return os << static_cast<const KGenParticle>(tau)
 		<< " visible=" << static_cast<const KLV>(tau.visible)
-		<< " decayMode=" << tau.decayMode << " vertex=" << tau.vertex;
+		<< " decayMode=" << int(tau.decayMode) << " vertex=" << tau.vertex;
 }
 
 std::ostream &operator<<(std::ostream &os, const KVertex &vertex)
@@ -178,6 +204,20 @@ std::ostream &operator<<(std::ostream &os, const KTaupairVerticesMap &pvs)
 {
 	for(size_t i = 0; i < pvs.diTauKey.size(); ++i)
 		os << "diTauKey=" << pvs.diTauKey[i] << " Vertex:" << pvs.vertices[i] << std::endl;
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const KTriggerObjects &to)
+{
+	os << "Trigger objects: Indices";
+	if (to.trgObjects.size() != to.toIdxFilter.size())
+		os << "\nTrigger objects are broken!" << std::endl;
+	for(size_t i = 0; i < to.trgObjects.size(); ++i)
+	{
+		os << "\n\t" << to.trgObjects[i] << ": ";
+		for (size_t j=0; j < to.toIdxFilter[i].size(); ++j)
+			os << to.toIdxFilter[i][j] << ", ";
+	}
 	return os;
 }
 
@@ -206,6 +246,53 @@ std::ostream &operator<<(std::ostream &os, const KFilterSummary &filter)
 		<< " decision=" << std::bitset<16>(filter.decision);
 }
 
+std::ostream &operator<<(std::ostream &os, const KHCALNoiseSummary &noise)
+{
+	os << "badRBXTS=" << noise.hasBadRBXTS4TS5;
+	os << " iso-noise: E="<< noise.isolatedNoiseSumE;
+	os << " Et=" << noise.isolatedNoiseSumEt;
+	os << " nChannels=" << noise.numIsolatedNoiseChannels;
+	os << "; hit-time=" << noise.min25GeVHitTime;
+	os << ".." << noise.max25GeVHitTime;
+	os << " E2=" << noise.minE2Over10TS;
+	os << ".." << noise.maxE2Over10TS;
+	os << " RBXEMP>=" << noise.minRBXEMF;
+	os << " RBX-hits<=" << noise.maxRBXHits;
+	os << " HPD-hits<=" << noise.maxHPDHits;
+	os << " HPD-other<=" << noise.maxHPDNoOtherHits;
+	os << " n0<=" << noise.maxZeros;
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const KL1Muon &mu)
+{
+	return os << static_cast<const KLV>(mu) << " state=" << std::bitset<16>(mu.state);
+}
+
+std::ostream &operator<<(std::ostream &os, const KMuon &mu)
+{
+	os << static_cast<const KLepton>(mu) << std::endl;
+	os << "\tglobal track: " << mu.globalTrack << std::endl;
+	os << "\tbest track: dxy=" << mu.dxy << " dz=" << mu.dz << std::endl;
+	os << "\tiso: track=" << mu.trackIso << " ecal=" << mu.ecalIso;
+	os << " hcal=" << mu.hcalIso << " pfIso03=" << mu.pfIso03 << std::endl;
+	os << "\tnChambers=" << mu.nChambers << " nMatches=" << mu.nMatches;
+	os << " nStations=" << mu.nMatchedStations;
+	os << " type=" << int(mu.type) << std::endl;
+	os << "\tpropagated: eta=" << mu.eta_propagated << " phi=" << mu.phi_propagated << std::endl;
+	os << "\thltMatch=" << std::bitset<64>(mu.hltMatch);
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const KMuonTriggerCandidate &cand)
+{
+	os << static_cast<const KTrack>(cand);
+	os << " decision=" << cand.isoDecision << " quantity=" << cand.isoQuantity;
+	return os;
+}
+
+
+
 ////////////////////////////////////////////////////////////
 // KMetadata.h
 ////////////////////////////////////////////////////////////
@@ -220,9 +307,11 @@ std::ostream &operator<<(std::ostream &os, const KProvenance &p)
 
 std::ostream &operator<<(std::ostream &os, const KLumiInfo &info)
 {
-	os << "Run:LS=" << info.nRun << ":" << info.nLumi << " flags=" << info.bitsUserFlags << std::endl;
-	//if (info.hltNames.size() > 0)
-		os << "\tHLTs(prescale)=";
+	os << "Run:LS=" << info.nRun << ":" << info.nLumi << " flags=" << info.bitsUserFlags;
+	if (info.hltNames.size() != info.hltPrescales.size())
+		return os << "\nHLT prescales corrupted!" << std::endl;
+	if (info.hltNames.size() > 0)
+		os << "\n\tHLTs(prescale)=";
 	for (size_t i = 0; i < info.hltNames.size(); ++i)
 		os << info.hltNames[i] << "(" << info.hltPrescales[i] << "), ";
 	return os;
@@ -259,16 +348,49 @@ std::ostream &operator<<(std::ostream &os, const KEventInfo &i)
 std::ostream &operator<<(std::ostream &os, const KTauMetadata &m)
 {
 	for (std::vector<std::string>::const_iterator it = m.binaryDiscriminatorNames.begin(); it != m.binaryDiscriminatorNames.end(); ++it)
-		os << "Binary tau discriminator: " << *it << std::endl;
+		os << "tau discriminator (binary): " << *it << std::endl;
 	for (std::vector<std::string>::const_iterator it = m.floatDiscriminatorNames.begin(); it != m.floatDiscriminatorNames.end(); ++it)
-		os << "Float tau discriminator: " << *it << std::endl;
+		os << "tau discriminator (float):  " << *it << std::endl;
 	return os;
 }
 
 std::ostream &operator<<(std::ostream &os, const KMuonMetadata &m)
 {
 	for (std::vector<std::string>::const_iterator it = m.hltNames.begin(); it != m.hltNames.end(); ++it)
-		os << "Muon HLT trigger objects: " << *it << std::endl;
+		os << "muon HLT trigger objects: " << *it << std::endl;
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const KElectronMetadata &m)
+{
+	for (std::vector<std::string>::const_iterator it = m.idNames.begin(); it != m.idNames.end(); ++it)
+		os << "electron IDs: " << *it << std::endl;
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const KJetMetadata &m)
+{
+	for (std::vector<std::string>::const_iterator it = m.tagNames.begin(); it != m.tagNames.end(); ++it)
+		os << "jet tags (float): " << *it << std::endl;
+	for (std::vector<std::string>::const_iterator it = m.idNames.begin(); it != m.idNames.end(); ++it)
+		os << "jet IDs (binary): " << *it << std::endl;
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const KTriggerObjectMetadata &m)
+{
+	os << "Trigger menu: '" << m.menu << "'";
+	if (m.toFilter.size() > 0)
+		os << " filters:" << std::endl;
+	for (size_t i=0; i < m.toFilter.size(); ++i)
+		os << "Trigger object filter (n): " << m.toFilter[i] << " (" << m.nFiltersPerHLT[i] << ")" << std::endl;
+	return os;
+}
+
+std::ostream &operator<<(std::ostream &os, const KFilterMetadata &m)
+{
+	for (std::vector<std::string>::const_iterator it = m.filternames.begin(); it != m.filternames.end(); ++it)
+		os << "filter names: " << *it << std::endl;
 	return os;
 }
 
@@ -276,7 +398,8 @@ std::ostream &operator<<(std::ostream &os, const KGenEventInfo &i)
 {
 	os << static_cast<const KEventInfo>(i) << std::endl;
 	os << "\tweight=" << i.weight << " bin=" << i.binValue << " alphaQCD=" << i.alphaQCD << std::endl;
-	os << "\tnPU(-2..2)=(" << int(i.nPUm2) << ", " << i.nPUm1 << int(i.nPU) << ", " << int(i.nPUp1) << ", " << int(i.nPUp2) << ") nPUMean=" << i.nPUMean;
+	os << "\tnPU=(" << int(i.nPUm2) << "--, " << int(i.nPUm1) << "-, ";
+	os << int(i.nPU) << ", " << int(i.nPUp1) << "+, " << int(i.nPUp2) << "++) nPUMean=" << i.nPUMean;
 	return os;
 }
 
