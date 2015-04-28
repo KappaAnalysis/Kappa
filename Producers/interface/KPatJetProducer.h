8ae9dd7a6a46452add98296d11c3a02b417c8666
Raphael Friese <raphael.friese@cern.ch>
2015-04-29 01:14:22 +0200

    prepare for genJets. Some problems popped up, might be solved already with newer MCs.

--- Producers/interface/KPatJetProducer.h
+++ Producers/interface/KPatJetProducer.h
@@ -10,13 +10,18 @@
 #define KAPPA_PATJETPRODUCER_H
 
 #include "KBaseMultiLVProducer.h"
+#include "KGenJetProducer.h"
 #include <DataFormats/PatCandidates/interface/Jet.h>
 
 class KPatJetProducer : public KBaseMultiLVProducer<edm::View<pat::Jet>, KJets >
 {
 public:
 	KPatJetProducer(const edm::ParameterSet &cfg, TTree *_event_tree, TTree *_run_tree) :
-		KBaseMultiLVProducer<edm::View<pat::Jet>, KJets>(cfg, _event_tree, _run_tree, getLabel()) {}
+		KBaseMultiLVProducer<edm::View<pat::Jet>, KJets>(cfg, _event_tree, _run_tree, getLabel())
+	{
+		genJet = new KGenJet;
+		_event_tree->Bronch("genJet", "KGenJet", &genJet);
+	}
 
 	static const std::string getLabel() { return "PatJets"; }
 
@@ -34,7 +39,21 @@ public:
 		out.electronFraction = in.electronEnergyFraction();
 		out.hfHadronFraction = in.HFHadronEnergyFraction();
 		out.hfEMFraction = in.HFEMEnergyFraction();
+
+
+		//write out generator information
+		//todo: catch if some daughters of genJets are not in genParticles collection
+		//in this case, the determination of the decay mode fails and the cmsRun exits with "product not found"
+		/*
+		const reco::GenJet* recoGenJet = in.genJet();
+		if (recoGenJet == NULL) // catch if null pointer is delivered from pat::Jet::genJet() and use an empty GenJet instead
+			recoGenJet = new reco::GenJet;
+		std::cout << recoGenJet->isJet() << std::endl;
+		KGenJetProducer::fillGenJet(*recoGenJet, *genJet); */
 	}
+private:
+	KGenJet* genJet;
+
 };
 
 #endif
