4b8b27b8aa1a7c4ccc5a66a94eced40e2c82e857
Joram Berger <joram.berger@cern.ch>
2015-06-19 18:56:22 +0200

    Test: Improved version of the test status script
    Use with watch

--- DataFormats/test/teststatus.sh
+++ DataFormats/test/teststatus.sh
@@ -1,6 +1,32 @@
-for i in $(ls -rt *txt)
-do
-  [[ $i != *"log.txt" ]] && (
-  grep "SUCCESS" $i >> /dev/null && echo "+ $i" || echo "- $i"
-  )
-done
+#!/bin/bash
+# Check the outputs of the Kappa test suite live
+# Usage: watch teststatus.sh
+
+if ! $(ls *txt &> /dev/null)
+then
+  echo "Kappa test not running here"
+  exit
+fi
+
+checkresult () {
+  echo "--------------------------------------------------------------"
+
+  for i in $(ls -rt *txt)
+  do
+    [[ $i != *"log.txt" ]] && (
+    grep "SUCCESS" $i >> /dev/null && echo "+ $i" || echo "- $i"
+    )
+  done
+  echo "--------------------------------------------------------------"
+}
+
+if [[ -f result.html ]]
+then
+  echo "Kappa test done                  $(date)"
+  checkresult
+else
+  echo "Kappa test running               $(date)"
+  checkresult
+  echo "Latest log output:"
+  tail -8 $(ls -rt1 | tail -1)
+fi
