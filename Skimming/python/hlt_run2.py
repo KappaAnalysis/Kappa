0bcec29745ece946eeb024b6d91912e290f345ac
Thomas Mueller <tmuller@cern.ch>
2015-08-27 14:02:55 +0200

    hlt_run2.py: change to more general version of regex. These catch ~64 HLTs, which might be dangerous in the future.

--- Skimming/python/hlt_run2.py
+++ Skimming/python/hlt_run2.py
@@ -3,37 +3,31 @@ hltWhitelist = cms.vstring( ## HLT selection
 	# https://twiki.cern.ch/twiki/bin/viewauth/CMS/HiggsToTauTauWorking2015#Trigger_Information
 	# can be tested at https://regex101.com
 	
-	# m
-	"^HLT_(Iso)?Mu[0-9]+(_TrkIsoVVL)?(_eta2p1)?_v[0-9]+$"
-	
-	# mm
-	"^HLT_(Iso)?Mu[0-9]+(_TrkIsoVVL)?(_eta2p1)?_(Iso)?Mu[0-9]+(_TrkIsoVVL)?(_eta2p1)?_v[0-9]+$"
-	
-	# e
-	"^HLT_Ele[0-9]+(_CaloIdL)?(_TrackIdL)?(_IsoVL)?(_eta2p1)?(_WP([0-9]+)?(Loose|Medium|Tight)?)?(_Gsf)?_v[0-9]+$",
-	
-	# ee
-	"^HLT_DoubleEle[0-9]+(_CaloIdL)?(_IsoVL)?(_eta2p1)?(_WP([0-9]+)?(Loose|Medium|Tight)?)?(_GsfTrkIdVL)?(_MW)?_v[0-9]+$",
-	"^HLT_Ele[0-9]+(_CaloIdL)?(_TrackIdL)?(_IsoVL)?(_eta2p1)?(_WP([0-9]+)?(Loose|Medium|Tight)?)?(_Gsf)?_^HLT_Ele[0-9]+(_CaloIdL)?(_TrackIdL)?(_IsoVL)?(_eta2p1)?(_WP([0-9]+)?(Loose|Medium|Tight)?)?(_Gsf)?_v[0-9]+$",
-	
-	# em
-	"^HLT_Mu[0-9]+(_TrkIsoVVL)_(Ele)[0-9]+(_Gsf)?_CaloId(L)?_TrackId(L)?_Iso(VL)?(_MediumWP)?_v[0-9]+$",
-	
-	# et
-	"^HLT_Ele[0-9]+(_CaloIdL)?(_TrackIdL)?(_IsoVL)?(_eta2p1)?(_WP([0-9]+)?(Loose|Medium|Tight)?)?(_Gsf)?_(Loose|Medium|Tight)?IsoPFTau[0-9]+_v[0-9]+$"
-	
-	# mt
-	"^HLT_(Iso)?Mu[0-9]+(_eta2p1)?_IterTrk[0-9]+_v[0-9]+$",
-	"^HLT_(Iso)?Mu[0-9]+(_eta2p1)?_(Loose|Medium|Tight)IsoPFTau[0-9]+(_Trk[0-9])?(_eta2p1)?(_Reg)?_v[0-9]+$",
-	
-	# tt
-	"^HLT_Double(Loose|Medium|Tight)?IsoPFTau[0-9]+(_Trk[0-9]+)?(_eta2p1)?(_Reg)?_v[0-9]+$",
-	
-	# t
-	"^HLT_(Loose|Medium|Tight)?IsoPFTau[0-9]+(_Trk[0-9]+)?(_eta2p1)?(_Reg)?_v[0-9]+$"
-	
-	# specials (possible generalization: Mu15, L1ETM20, Photon20, Ele8)
-	"^HLT_(Loose|Medium)?IsoPFTau[0-9]+_Trk[0-9]+(_eta2p1)?_MET[0-9]+_v[0-9]+$",
+	"^HLT_.*Mu[0-9]+.*$",
+	"^HLT_.*Ele[0-9]+.*$",
+	"^HLT_.*Tau[0-9]+.*$",
 )
 
-hltBlacklist = cms.vstring()
+hltBlacklist = cms.vstring(
+	"^HLT_.*Photon.*$",
+	"^HLT_.*photon.*$",
+	"^HLT_.*Jet.*$",
+	"^HLT_.*jet.*$",
+	"^HLT_.*MET.*$",
+	"^HLT_.*Psi.*$",
+	"^HLT_.*Mass.*$",
+	"^HLT_.*HT[0-9]+.*$",
+	"^HLT_L1.*$",
+	"^HLT_L2.*$",
+	"^HLT_L1L2.*$",
+	"^HLT_.*SameSign.*$",
+	"^HLT_.*NoVertex.*$",
+	"^HLT_.*NoVtx.*$",
+	"^HLT_.*NoFilters.*$",
+	"^HLT_.*SaveObjects.*$",
+	"^HLT_.*_Di.*$",
+	"^HLT_.*_Double.*$",
+	"^HLT_.*_Bs.*$",
+	"^HLT_.*_DZ.*$",
+	"^HLT_.*Onia.*$",
+)
