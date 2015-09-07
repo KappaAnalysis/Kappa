ce27e9bb09d5987826f1c43e3e1ca52a3a21c075
Fabio Colombo <fabio.colombo@cern.ch>
2015-09-07 17:51:07 +0200

    KMET_run2: move to 25ns weight files

--- Skimming/python/KMET_run2_cff.py
+++ Skimming/python/KMET_run2_cff.py
@@ -143,10 +143,10 @@ from RecoJets.JetProducers.PileupJetIDParams_cfi import JetIdParams
 from RecoMET.METPUSubtraction.mvaPFMET_cff import pfMVAMEt as pfMetMVA
 
 pfMetMVA.inputFileNames = cms.PSet(
-        U     = cms.FileInPath('RecoMET/METPUSubtraction/data/gbru_7_4_X_miniAOD_50NS_July2015.root'),
-        DPhi  = cms.FileInPath('RecoMET/METPUSubtraction/data/gbrphi_7_4_X_miniAOD_50NS_July2015.root'),
-        CovU1 = cms.FileInPath('RecoMET/METPUSubtraction/data/gbru1cov_7_4_X_miniAOD_50NS_July2015.root'),
-        CovU2 = cms.FileInPath('RecoMET/METPUSubtraction/data/gbru2cov_7_4_X_miniAOD_50NS_July2015.root') )
+        U     = cms.FileInPath('RecoMET/METPUSubtraction/data/gbru_7_4_X_miniAOD_25NS_July2015.root'),
+        DPhi  = cms.FileInPath('RecoMET/METPUSubtraction/data/gbrphi_7_4_X_miniAOD_25NS_July2015.root'),
+        CovU1 = cms.FileInPath('RecoMET/METPUSubtraction/data/gbru1cov_7_4_X_miniAOD_25NS_July2015.root'),
+        CovU2 = cms.FileInPath('RecoMET/METPUSubtraction/data/gbru2cov_7_4_X_miniAOD_25NS_July2015.root') )
 
 metMVAEM = cms.EDProducer('PFMETProducerMVATauTau', 
                              **pfMetMVA.parameters_())#pfMVAMEt.clone()
