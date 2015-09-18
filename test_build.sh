b78597851f705d71a51f6724b868afdf1f9eee16
Georg Sieber <g.sieber@gmail.com>
2015-09-18 14:43:17 +0200

    add travis docker stuff

---
+++ test_build.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+/etc/cvmfs/run-cvmfs.sh
+
+export SCRAM_ARCH=slc6_amd64_gcc481
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+
+mkdir -p /home/build && cd /home/build
+
+
+. $VO_CMS_SW_DIR/cmsset_default.sh
+scram project ${TEST_CMSSW_VERSION}
+
+cd ${TEST_CMSSW_VERSION}
+
+cmsenv
+
+cd src/
+
+mkdir Kappa
+mv /home/travis/* Kappa/
+
+echo "# ================= #"
+echo "# Building in CMSSW #"
+echo "# ================= #"
+
+scram b -v -j 2 || exit 1
+
+echo "# =================== #"
+echo "# Building standalone #"
+echo "# =================== #"
+
+echo $CMSSW_BASE
+make -C Kappa/DataFormats/test -j2 || exit 1
