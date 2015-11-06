2c249c8154b1e4487cf56bf95fc3fdc1962223f4
Raphael Friese <raphael.friese@cern.ch>
2015-11-06 16:37:26 +0100

    collection with all available Higgses with 125GeV

--- Skimming/python/datasets2015_miniAODv2.py
+++ Skimming/python/datasets2015_miniAODv2.py
@@ -8,5 +8,10 @@ queries = {
 		"process" : "VBFHToTauTau",
 		"energy" : "13",
 		"campaign" : "RunIISpring15MiniAODv2"
+	},
+	"SM_HiggsM125_miniAODv2": {
+		"process" : ".*HToTauTau.*M125",
+		"energy" : "13",
+		"campaign" : "RunIISpring15MiniAODv2"
 	}
 }
