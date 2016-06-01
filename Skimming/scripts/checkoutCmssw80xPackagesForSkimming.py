3620de3aab44365aa8f43b78f26736d7b0a3a036
Raphael Friese <raphael.friese@cern.ch>
2016-06-01 14:16:44 +0200

    first version of 80X checkout script

---
+++ Skimming/scripts/checkoutCmssw80xPackagesForSkimming.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#-# Copyright (c) 2014 - All Rights Reserved
+#-#   Raphael Friese <Raphael.Friese@cern.ch>
+#-#   Stefan Wayand <stefan.wayand@gmail.com>
+#-#   Thomas Mueller <tmuller@cern.ch>
+
+# checkout script for cmssw53x for skimming
+# todo: implement logger
+# todo: make cmsenv/scramv work
+
+import os
+import sys
+from optparse import OptionParser
+
+#################################################################################################################
+
+
+def execCommands(commands):
+	for command in commands:
+		print ""
+		print "command: " + command
+		exitCode = 1
+		nTrials = 0
+		while exitCode != 0:
+			if nTrials > 1:
+				print "Last command could NOT be executed successfully! Stop program!"
+				sys.exit(1)
+
+				#logger.info("{CHECKOUT_COMMAND} (trial {N_TRIAL}):").formal(CHECKOUT_COMMAND = command, N_TRIAL = (nTrials+1))
+
+			if command.startswith("cd"):
+				os.chdir(os.path.expandvars(command.replace("cd ", "")))
+				exitCode = int((os.path.expandvars(command.replace("cd ", "").strip("/")) != os.getcwd().strip("/")))
+			else:
+				exitCode = os.system(command)
+
+			nTrials += 1
+
+	return
+
+#################################################################################################################
+
+
+def getSysInformation():
+	sysInformation = {
+	"github_username": os.popen("git config user.name").readline().replace("\n", ""),
+	"email": os.popen("git config user.email").readline().replace("\n", ""),
+	"editor": os.popen("git config core.editor").readline().replace("\n", ""),
+	"pwd": os.getcwd()
+	}
+	return sysInformation
+
+
+#################################################################################################################
+
+
+def checkoutPackages(args):
+	commands = [
+		"cd " + os.path.expandvars("$CMSSW_BASE/src/"),
+		"git cms-merge-topic -u KappaAnalysis:CMSSW_8010",
+		"git cms-addpkg RecoMET/METPUSubtraction",
+		"git cms-addpkg DataFormats/METReco",
+		#Electron cutBased Id and MVA Id
+		#https://twiki.cern.ch/twiki/bin/view/CMS/CutBasedElectronIdentificationRun2#Recipe_for_regular_users_for_747
+		#https://twiki.cern.ch/twiki/bin/view/CMS/MultivariateElectronIdentificationRun2#Recipes_for_747_Spring15_MVA_tra
+		#"git cms-merge-topic ikrav:egm_id_747_v2",
+
+		#Check out Kappa
+		"git clone https://github.com/KappaAnalysis/Kappa.git -b CMSSW_8_0_X"
+	]
+	execCommands(commands)
+	return
+
+
+#################################################################################################################
+
+
+def main():
+	parser = OptionParser()
+	sysInformation = getSysInformation()
+
+	parser.add_option("--github_username", help="your name as in github. Default: " + sysInformation["github_username"], default=sysInformation["github_username"], nargs='?')
+	parser.add_option("--mail", help="your email. Default: " + sysInformation["email"], default=sysInformation["email"], nargs='?')
+	parser.add_option("--editor", help="your favorite editor (ex. emacs). Default: " + sysInformation["editor"], default=sysInformation["editor"], nargs='?')
+	parser.add_option("--cmssw_version", help="the CMSSW Version to checko out. Default: CMSSW_7_2_2", default="CMSSW_7_2_2", nargs='?')
+
+	#args = parser.parse_args()
+	(options, args) = parser.parse_args()
+	checkoutPackages(args)
+
+
+#################################################################################################################
+
+
+if __name__ == "__main__":
+	main()
