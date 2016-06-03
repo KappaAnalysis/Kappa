66ac1f2bcb588bff88a276cbac4553b7a30f727f
Gregor Koehler <uxcwe@student.kit.edu>
2016-06-03 16:30:55 +0200

    Spring 16 MC

---
+++ Skimming/python/datasets2016_Spring16.py
@@ -0,0 +1,32 @@
+queries = {
+	"Spring16_DYJets": {
+		"process" : "^DY([0-9])?JetsTo(LL|TauTau)",
+		"campaign" : "RunIISpring16MiniAODv1",
+		"energy" : "13"
+	},
+	"Spring16_Dibosons": {
+		"process" : "(W|Z|V)(W|Z|V)(Jets)?(To)?",
+		"campaign" : "RunIISpring16MiniAODv1",
+		"energy" : "13"
+	},
+	"Spring16_QCD": {
+		"process" : "^QCD",
+		"campaign" : "RunIISpring16MiniAODv1",
+		"energy" : "13"
+	},
+	"Spring16_TTJets": {
+		"process" : "^TTJets|^Tbar|^T_tW|^TT|(^ST.*)$",
+		"campaign" : "RunIISpring16MiniAODv1",
+		"energy" : "13"
+	},
+	"Spring16_WJets": {
+		"process" : "(^W(\d)Jets|^WJets)ToLNu",
+		"campaign" : "RunIISpring16MiniAODv1",
+		"energy" : "13"
+	},
+	"Spring16_EWK": {
+		"process" : "^EWK(WMinus|WPlus|Z)2Jets",
+		"campaign" : "RunIISpring16MiniAODv1",
+		"energy" : "13"
+	}
+}
