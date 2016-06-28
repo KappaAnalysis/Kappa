9c3659e8f2470d72f1cf5948daf5a74c23cce888
Raphael Friese <raphael.friese@cern.ch>
2016-06-28 19:32:13 +0200

    addXSec with suggestion from previously known samples to speed up the process a little bit

--- Skimming/scripts/addXSec.py
+++ Skimming/scripts/addXSec.py
@@ -9,12 +9,36 @@ import os
 import string
 from pprint import pprint
 import json
-
+import sys
 from Kappa.Skimming.registerDatasetHelper import *
 #from Kappa.Skimming.equationparsing import eval_expr
 cmssw_base = os.environ.get("CMSSW_BASE")
 dataset = os.path.join(cmssw_base, "src/Kappa/Skimming/data/datasets.json")
+import pprint
 
+def suggestion(dict, sample):
+	# aus sample process extrahieren, schwerpunktsenergie
+	query = { "energy" : get_energy(sample, "2016"), "process" : get_process(sample).replace("/", "") }
+	print "searching for previously known cross-sections with following query: "
+	pprint.pprint(query)
+	query_result = []
+	best_guess = 0
+	for sample, values in dict.iteritems():
+		matches = True
+		for name, attribute in values.iteritems():
+			if not name in query: continue
+			if not (re.match(str(query[name]), str(attribute)) != None):#query[name] != attribute: 
+				matches = False
+		if matches and "xsec" in dict[sample]:
+			query_result.append("\t" + make_nickname(values) + " : " + str(dict[sample]["xsec"]))
+			if best_guess == 0:
+				best_guess = dict[sample]["xsec"]
+			else:
+				if best_guess != dict[sample]["xsec"]:
+					best_guess = -1 # differences, no recommendation found
+	print "previously set: "
+	pprint.pprint( query_result )
+	return best_guess
 
 
 def main():
@@ -28,11 +52,21 @@ def main():
 
 	(options, args) = parser.parse_args()
 	sample = options.sample
-	xsec = options.xsec
+	xsec = float(options.xsec)
 	#xsec = eval_expr(xsec.replace('x', '*').replace(' ', ''))
 	dict = load_database(dataset)
+	if "xsec" in dict[sample]:
+		print "sample already does have a cross-section of " + str(dict[sample]["xsec"]) + ". Stopping"
+		sys.exit()
+	best_guess = suggestion(dict, sample)
+	if best_guess > 0 and xsec < 0:
+		print "automatically fetching cross-section from previously known samples"
+		xsec = best_guess
+	if xsec <= 0.0:
+		print "no proper cross-section defined. Stopping"
+		sys.exit()
 	dict[sample]["xsec"] = float(xsec)
-	import pprint
+	print "\n new datasets.json entry: "
 	pprint.pprint( dict[sample])
 	save_database(dict, dataset)
 
