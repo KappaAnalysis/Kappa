02b8608630c71dd1a478bb32b1efbbea85bcd59b
Georg Sieber <g.sieber@gmail.com>
2016-07-11 14:59:53 +0200

    try to explicitly setting scram arch.

--- test_build.sh
+++ test_build.sh
@@ -2,7 +2,7 @@
 
 /etc/cvmfs/run-cvmfs.sh
 
-export SCRAM_ARCH=slc6_amd64_gcc481
+# export SCRAM_ARCH=slc6_amd64_gcc481
 export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
 
 mkdir -p /home/build && cd /home/build
