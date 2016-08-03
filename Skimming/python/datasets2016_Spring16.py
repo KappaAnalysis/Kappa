f02eaaf55716f73aec6f90b34302dac56eaa9e08
Raphael Friese <raphael.friese@cern.ch>
2016-08-03 14:05:51 +0200

    new reHLT samples and dropping most of miniaod v1

--- Skimming/python/datasets2016_Spring16.py
+++ Skimming/python/datasets2016_Spring16.py
@@ -6,12 +6,12 @@ queries = {
 	},
 	"Spring16_DYJets": {
 		"process" : "^DY([0-9])?JetsTo(LL|TauTau)",
-		"campaign" : "RunIISpring16MiniAODv.*",
+		"campaign" : "RunIISpring16MiniAODv2",
 		"energy" : "13"
 	},
 	"Spring16_Dibosons": {
 		"process" : "(W|Z|V)(W|Z|V)(Jets)?(To)?",
-		"campaign" : "RunIISpring16MiniAODv.*",
+		"campaign" : "RunIISpring16MiniAODv2",
 		"energy" : "13"
 	},
     "Spring16_SUSY_Sync": {
@@ -21,32 +21,32 @@ queries = {
     },
     "Spring16_SUSY": {
             "process" : "SUSY",
-            "campaign" : "RunIISpring16MiniAODv.*",
+            "campaign" : "RunIISpring16MiniAODv(1|2)$",
             "energy" : "13"
     },
     "Spring16_SM_Higgs": {
-            "process" : "(GluGlu|VBF|Wplus|Wminus|Z)HToTauTau",
-            "campaign" : "RunIISpring16MiniAODv.*",
+            "process" : "((GluGlu|VBF|Wplus|Wminus|Z)HToTauTau)|(ttHJetToTT)",
+            "campaign" : "RunIISpring16MiniAOD.*reHLT",
             "energy" : "13"
     },
 	"Spring16_QCD": {
 		"process" : "^QCD",
-		"campaign" : "RunIISpring16MiniAODv.*",
+		"campaign" : "RunIISpring16MiniAODv2",
 		"energy" : "13"
 	},
 	"Spring16_TTJets": {
 		"process" : "^TTJets|^Tbar|^T_tW|^TT|(^ST.*)$",
-		"campaign" : "RunIISpring16MiniAODv.*",
+		"campaign" : "RunIISpring16MiniAODv2",
 		"energy" : "13"
 	},
 	"Spring16_WJets": {
 		"process" : "(^W(\d)Jets|^WJets)ToLNu",
-		"campaign" : "RunIISpring16MiniAODv.*",
+		"campaign" : "RunIISpring16MiniAODv2",
 		"energy" : "13"
 	},
 	"Spring16_EWK": {
 		"process" : "^EWK(WMinus|WPlus|Z)2Jets",
-		"campaign" : "RunIISpring16MiniAODv.*",
+		"campaign" : "RunIISpring16MiniAODv2",
 		"energy" : "13"
 	},
 	"Spring16_reHLT": {
