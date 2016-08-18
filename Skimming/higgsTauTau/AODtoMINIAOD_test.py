a4aaa3d5c5293a3f74d3e847b2133ddcb0485ba2
hale sert <sert@physik.rwth-aachen.de>
2016-08-18 13:30:07 +0200

    PrivateMiniAOD studies: additional files necessary for it

---
+++ Skimming/higgsTauTau/AODtoMINIAOD_test.py
@@ -0,0 +1,108 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: miniAOD-prod -s PAT --eventcontent MINIAODSIM --runUnscheduled --mc --filein file:root://cms-xrd-global.cern.ch//store/mc/RunIIFall15DR76/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/AODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext1-v1/00000/00120DE2-D9CE-E511-BACE-0025904C51DA.root --fileout file:miniAOD_fromAOD.root --conditions 76X_mcRun2_asymptotic_RunIIFall15DR76_v1 -n 100 --no_exec --python_filename=AODtoMINIAOD_test.py
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process('PAT')
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('SimGeneral.MixingModule.mixNoPU_cfi')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('PhysicsTools.PatAlgos.slimming.metFilterPaths_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:root://cms-xrd-global.cern.ch//store/mc/RunIIFall15DR76/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/AODSIM/PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext1-v1/00000/00120DE2-D9CE-E511-BACE-0025904C51DA.root'),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+    allowUnscheduled = cms.untracked.bool(True)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('miniAOD-prod nevts:100'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.MINIAODSIMoutput = cms.OutputModule("PoolOutputModule",
+    compressionAlgorithm = cms.untracked.string('LZMA'),
+    compressionLevel = cms.untracked.int32(4),
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string(''),
+        filterName = cms.untracked.string('')
+    ),
+    dropMetaData = cms.untracked.string('ALL'),
+    eventAutoFlushCompressedSize = cms.untracked.int32(15728640),
+    fastCloning = cms.untracked.bool(False),
+    fileName = cms.untracked.string('file:miniAOD_fromAOD.root'),
+    outputCommands = process.MINIAODSIMEventContent.outputCommands,
+    overrideInputFileSplitLevels = cms.untracked.bool(True)
+)
+
+# Additional output definition
+
+# Other statements
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '76X_mcRun2_asymptotic_RunIIFall15DR76_v1', '')
+
+# Path and EndPath definitions
+process.Flag_trackingFailureFilter = cms.Path(process.goodVertices+process.trackingFailureFilter)
+process.Flag_goodVertices = cms.Path(process.primaryVertexFilter)
+process.Flag_CSCTightHaloFilter = cms.Path(process.CSCTightHaloFilter)
+process.Flag_trkPOGFilters = cms.Path(process.trkPOGFilters)
+process.Flag_trkPOG_logErrorTooManyClusters = cms.Path(~process.logErrorTooManyClusters)
+process.Flag_EcalDeadCellTriggerPrimitiveFilter = cms.Path(process.EcalDeadCellTriggerPrimitiveFilter)
+process.Flag_ecalLaserCorrFilter = cms.Path(process.ecalLaserCorrFilter)
+process.Flag_trkPOG_manystripclus53X = cms.Path(~process.manystripclus53X)
+process.Flag_eeBadScFilter = cms.Path(process.eeBadScFilter)
+process.Flag_METFilters = cms.Path(process.metFilters)
+process.Flag_chargedHadronTrackResolutionFilter = cms.Path(process.chargedHadronTrackResolutionFilter)
+process.Flag_CSCTightHaloTrkMuUnvetoFilter = cms.Path(process.CSCTightHaloTrkMuUnvetoFilter)
+process.Flag_HBHENoiseIsoFilter = cms.Path(process.HBHENoiseFilterResultProducer+process.HBHENoiseIsoFilter)
+process.Flag_hcalLaserEventFilter = cms.Path(process.hcalLaserEventFilter)
+process.Flag_HBHENoiseFilter = cms.Path(process.HBHENoiseFilterResultProducer+process.HBHENoiseFilter)
+process.Flag_trkPOG_toomanystripclus53X = cms.Path(~process.toomanystripclus53X)
+process.Flag_EcalDeadCellBoundaryEnergyFilter = cms.Path(process.EcalDeadCellBoundaryEnergyFilter)
+process.Flag_HcalStripHaloFilter = cms.Path(process.HcalStripHaloFilter)
+process.Flag_muonBadTrackFilter = cms.Path(process.muonBadTrackFilter)
+process.Flag_CSCTightHalo2015Filter = cms.Path(process.CSCTightHalo2015Filter)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.MINIAODSIMoutput_step = cms.EndPath(process.MINIAODSIMoutput)
+
+# Schedule definition
+process.schedule = cms.Schedule(process.Flag_HBHENoiseFilter,process.Flag_HBHENoiseIsoFilter,process.Flag_CSCTightHaloFilter,process.Flag_CSCTightHaloTrkMuUnvetoFilter,process.Flag_CSCTightHalo2015Filter,process.Flag_HcalStripHaloFilter,process.Flag_hcalLaserEventFilter,process.Flag_EcalDeadCellTriggerPrimitiveFilter,process.Flag_EcalDeadCellBoundaryEnergyFilter,process.Flag_goodVertices,process.Flag_eeBadScFilter,process.Flag_ecalLaserCorrFilter,process.Flag_trkPOGFilters,process.Flag_chargedHadronTrackResolutionFilter,process.Flag_muonBadTrackFilter,process.Flag_trkPOG_manystripclus53X,process.Flag_trkPOG_toomanystripclus53X,process.Flag_trkPOG_logErrorTooManyClusters,process.Flag_METFilters,process.endjob_step,process.MINIAODSIMoutput_step)
+
+#do not add changes to your config after this point (unless you know what you are doing)
+from FWCore.ParameterSet.Utilities import convertToUnscheduled
+process=convertToUnscheduled(process)
+process.load('Configuration.StandardSequences.PATMC_cff')
+from FWCore.ParameterSet.Utilities import cleanUnscheduled
+process=cleanUnscheduled(process)
+
+# customisation of the process.
+
+# Automatic addition of the customisation function from PhysicsTools.PatAlgos.slimming.miniAOD_tools
+from PhysicsTools.PatAlgos.slimming.miniAOD_tools import miniAOD_customizeAllMC 
+
+#call to customisation function miniAOD_customizeAllMC imported from PhysicsTools.PatAlgos.slimming.miniAOD_tools
+process = miniAOD_customizeAllMC(process)
+
+# End of customisation functions
---
+++ Skimming/higgsTauTau/crabConfig_privateMiniAOD_test.py
@@ -0,0 +1,113 @@
+#!/bin/env python
+# crab submission script
+# usage: python crabConfig.py submit
+
+from CRABClient.UserUtilities import getUsernameFromSiteDB
+from httplib import HTTPException
+from CRABAPI.RawCommand import crabCommand
+from CRABClient.ClientExceptions import ClientException
+from multiprocessing import Process
+from Kappa.Skimming.registerDatasetHelper import get_sample_by_nick
+from Kappa.Skimming.datasetsHelper2015 import isData
+import sys
+from glob import glob
+import os, shutil
+from Kappa.Skimming.tools import read_grid_control_includes
+import datetime
+today=datetime.date.today().strftime("%Y-%m-%d")
+date = today # feel free to change
+
+def submit(config):
+	try:
+		crabCommand('submit', config = config)
+	except HTTPException as hte:
+		print "Failed submitting task: %s" % (hte.headers)
+	except ClientException as cle:
+		print "Failed submitting task: %s" % (cle)
+
+def crab_command(command):
+	for dir in glob('/net/scratch_cms/institut_3b/%s/kappa/crab_kappa_skim-%s/*'%(getUsernameFromSiteDB(), date)):
+		try:
+			crabCommand(command, dir = dir)
+		except HTTPException as hte:
+			print hte
+
+def check_path(path):
+	if os.path.exists(path):
+		print(path + " already exists! Delete it now in order to re-initialize it by crab? [y/n]")
+		yes = set(['yes','y', 'ye', ''])
+		no = set(['no','n'])
+
+		choice = raw_input().lower()
+		if choice in yes:
+			shutil.rmtree(path)
+			return
+		elif choice in no:
+			return
+		else:
+			sys.stdout.write(path + " already exists! Delete it now in order to re-initialize it by crab?")
+
+def submission():
+	from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+	config = config()
+
+	##-- Your name of the crab project
+	config.General.requestName = 'KAPPA_FROM_AOD_skim_test'
+	#config.General.workArea = 'crab_projects'
+	config.General.workArea = '/net/scratch_cms/institut_3b/%s/kappa/crab_kappa_skim-%s'%(getUsernameFromSiteDB(), date)
+
+	##-- Transfer root files as well as log files "cmsRun -j FrameworkJobReport.xml" (log file = FrameworkJobReport.xml)
+	config.General.transferOutputs = True
+	config.General.transferLogs = True
+
+	##-- We want to have the special dcms role (better fair share at german grid sites). 
+	config.User.voGroup = 'dcms'
+
+	##-- the scripts (Analysis means with EDM input) which are executed. psetName is the cmsRun config and scriptExe is a shell config which should include "cmsRun -j FrameworkJobReport.xml -p PSet.py" (PSet.py is the renamed config.JobType.psetName)
+	config.JobType.pluginName = 'Analysis'
+	config.JobType.sendPythonFolder = True
+	config.JobType.psetName = 'AODtoMINIAOD_test.py'
+	config.JobType.scriptExe = 'kappaWorkflow_privateMiniAOD_test.sh'
+	#config.JobType.maxJobRuntimeMin = 2750
+	#config.JobType.maxMemoryMB = 2500
+
+	##-- instead of taking the outputfile per hand use the result of pset.py and renamed it, which cheat on the test of is an EDM file test and allows to use publish the data 
+	config.JobType.disableAutomaticOutputCollection = True
+	config.JobType.outputFiles = ['kappaTuple.root']
+
+	##-- The dataset you want to process:
+
+	config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIIFall15DR76-PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext1-v1/AODSIM'
+	config.Data.inputDBS = 'global'
+	config.Data.splitting = 'FileBased'
+	config.Data.unitsPerJob = 1
+
+	##-- If you want to run test jobs set totalUnits to a small number and publication to false
+	#config.Data.totalUnits = 1
+	config.Data.publication = False
+	
+	##-- the output strorage element
+	config.Site.storageSite = 'T2_DE_DESY'
+	config.Data.outLFNDirBase = '/store/user/%s/higgs-kit/skimming/80X_%s'%(getUsernameFromSiteDB(), date)
+	
+	##-- Run in xrootd mode (which allows you to run the jobs on all possible sites) 
+	config.Data.ignoreLocality = True
+	config.Site.whitelist = ['T2_CH_CERN','T2_DE_DESY','T1_DE_KIT','T2_DE_RWTH','T2_UK_London_IC', 'T2_US_MIT']
+	
+	
+	config.Site.storageSite = "T2_DE_DESY"
+
+	p = Process(target=submit, args=(config,))
+	p.start()
+	p.join()
+	
+if __name__ == "__main__":
+	if len(sys.argv) == 1: 
+		print "no setting provided"
+		sys.exit()
+	if sys.argv[1] == "submit":
+		submission()
+	elif sys.argv[1] in ["status", "resubmit", "kill"]:
+		crab_command(sys.argv[1])
+	else:
+		print "setting \"%s\" is not implemented"% sys.argv[1]
