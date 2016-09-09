b9b59d13ea2ceeef0801a8c7fe7fc8a984a618f8
stefan <stefan.wayand@cern.ch>
2016-09-09 13:40:59 +0200

    First test commit, with scram only working

--- DataFormats/test/standalone.h
+++
@@ -1,5 +0,0 @@
-//- Copyright (c) 2010 - All Rights Reserved
-//-  * Manuel Zeise <zeise@cern.ch>
-
-#define KAPPA_STANDALONE_DICT
-
--- Skimming/scripts/speedup_compile.sh
+++
@@ -1,5 +0,0 @@
-if [[ -e ${CMSSW_BASE}/config/toolbox/${SCRAM_ARCH}/tools/selected/gcc-cxxcompiler.xml ]]
-then
-  cp ${CMSSW_BASE}/config/toolbox/${SCRAM_ARCH}/tools/selected/gcc-cxxcompiler.xml ${CMSSW_BASE}/config/toolbox/${SCRAM_ARCH}/tools/selected/gcc-cxxcompiler.xml.backup
-  sed -i -e "s@-fipa-pta@@g" ${CMSSW_BASE}/config/toolbox/${SCRAM_ARCH}/tools/selected/gcc-cxxcompiler.xml
-fi
