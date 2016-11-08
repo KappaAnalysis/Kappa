82a67789bdbb93b63d0e43d9716dce7b303da74b
anehrkor <alexander.nehrkorn@cern.ch>
2016-11-08 16:41:46 +0100

    fix weird indentations in crabConfig.py part 2

--- Skimming/higgsTauTau/crabConfig.py
+++ Skimming/higgsTauTau/crabConfig.py
@@ -85,18 +85,18 @@ def submission(events_per_job):
 		config.Data.inputDBS = get_inputDBS_by_nick(nickname)
 		nfiles = get_n_files_from_nick(nickname)
 		if events_per_job:
-                    nevents = get_n_generated_events_from_nick(nickname)
-                    try:
-                        if int(nfiles) > 0 and int(nevents) > 0:
-                            files_per_job = int(events_per_job) * int(nfiles) / int(nevents)
-                            if files_per_job > 1:
-                                config.Data.unitsPerJob = files_per_job
-                    except:
-                        print "Its not possilbe to make ",events_per_job," events/job for ",nickname," which has Nevents:",nevents," and Nfiles",nfiles," in the database. Just make one file per job"
-        if float(config.Data.unitsPerJob) > 0 and float(nfiles)/float(config.Data.unitsPerJob) >= job_submission_limit:
-        	files_per_job = ceil(float(nfiles)/job_submission_limit)
-        	if files_per_job > 1:
-        		config.Data.unitsPerJob = files_per_job
+			nevents = get_n_generated_events_from_nick(nickname)
+			try:
+				if int(nfiles) > 0 and int(nevents) > 0:
+					files_per_job = int(events_per_job) * int(nfiles) / int(nevents)
+					if files_per_job > 1:
+						config.Data.unitsPerJob = files_per_job
+			except:
+				print "Its not possilbe to make ",events_per_job," events/job for ",nickname," which has Nevents:",nevents," and Nfiles",nfiles," in the database. Just make one file per job"
+		if float(config.Data.unitsPerJob) > 0 and float(nfiles)/float(config.Data.unitsPerJob) >= job_submission_limit:
+			files_per_job = ceil(float(nfiles)/job_submission_limit)
+			if files_per_job > 1:
+				config.Data.unitsPerJob = files_per_job
                         
 		config.JobType.pyCfgParams = ['globalTag=80X_dataRun2_Prompt_ICHEP16JEC_v0' if isData(nickname) else 'globalTag=80X_mcRun2_asymptotic_2016_miniAODv2_v1' ,'kappaTag=KAPPA_2_1_0','nickname=%s'%(nickname),'outputfilename=kappa_%s.root'%(nickname),'testsuite=False']
 		config.JobType.outputFiles = ['kappa_%s.root'%(nickname)]
