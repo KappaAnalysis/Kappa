80fc2181406901f49e3df4f8a7e8b80c9292ec00
anehrkor <alexander.nehrkorn@cern.ch>
2016-11-28 09:21:39 +0100

    modify checkout script for new Egamma MVA Id

--- Skimming/scripts/checkoutCmssw80xPackagesForSkimming.py
+++ Skimming/scripts/checkoutCmssw80xPackagesForSkimming.py
@@ -60,9 +60,9 @@ def checkoutPackages(args):
 		"cd " + os.path.expandvars("$CMSSW_BASE/src/"),
 		#Electron cutBased Id and MVA Id
 		#https://twiki.cern.ch/twiki/bin/view/CMS/CutBasedElectronIdentificationRun2#Recipe_for_regular_users_for_8_0
-		#https://twiki.cern.ch/twiki/bin/view/CMS/MultivariateElectronIdentificationRun2#Recipes_for_747_Spring15_MVA_tra
+		#https://twiki.cern.ch/twiki/bin/view/CMS/MultivariateElectronIdentificationRun2#Recipes_for_regular_users_common
 		#This needs to be checked out first since there are conflicts with MVA MET otherwise and then 63 packages are checked out...
-		"git cms-merge-topic ikrav:egm_id_80X_v1",
+		"git cms-merge-topic ikrav:egm_id_80X_v2",
 		# exact copy from https://twiki.cern.ch/twiki/bin/viewauth/CMS/HiggsToTauTauWorking2016#MET
 		"git cms-addpkg RecoMET/METPUSubtraction",
 		"git cms-addpkg DataFormats/METReco",
@@ -76,10 +76,18 @@ def checkoutPackages(args):
 		"sed '/import\ switchJetCollection/a from\ RecoMET\.METProducers\.METSignificanceParams_cfi\ import\ METSignificanceParams_Data' PhysicsTools/PatUtils/python/tools/runMETCorrectionsAndUncertainties.py -i",
 		"git clone https://github.com/artus-analysis/TauRefit.git VertexRefit/TauRefit",
 		#because of the MVA MET branch checkout we need to merge the EGamma branch into the mvamet one
-		"git merge ikrav/egm_id_80X_v1 --no-edit",
+		"git merge ikrav/egm_id_80X_v2 --no-edit",
 
 		#Check out Kappa
-		"git clone https://github.com/KappaAnalysis/Kappa.git"
+		"git clone https://github.com/KappaAnalysis/Kappa.git",
+		
+		#in order to complete recipe for electron MVA Id, we need to invoke scram b already in this script
+		"scram b -j 4",
+		"cd " + os.path.expandvars("$CMSSW_BASE/external/slc6_amd64_gcc530/"),
+		"git clone https://github.com/ikrav/RecoEgamma-ElectronIdentification.git data/RecoEgamma/ElectronIdentification/data",
+		"cd " + os.path.expandvars("$CMSSW_BASE/external/slc6_amd64_gcc530/data/RecoEgamma/ElectronIdentification/data"),
+		"git checkout egm_id_80X_v1",
+		"cd " + os.path.expandvars("$CMSSW_BASE/src/")
 	]
 	execCommands(commands)
 	return
