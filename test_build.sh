f8336af55309cd7da09bfa103ab88b55d9a56047
Olena Hlushchenko <olena.hlushchenko@physik.rwth-aachen.de>
2017-05-09 18:30:25 +0200

    TEST: correct the path, add cmsRun

--- test_build.sh
+++ test_build.sh
@@ -130,19 +130,20 @@ cd kappa_run
 echo "# =================== #"
 echo "# Test Config #"
 echo "# =================== #"
-ls $CMSSW_BASE/src
-ls $CMSSW_BASE/src/Kappa
-ls $CMSSW_BASE/src/Kappa/Skimming/
 ls $CMSSW_BASE/src/Kappa/Skimming/examples/
-head $CMSSW_BASE/Kappa/src/Skimming/examples/skim_tutorial1_basic.py
+cat $CMSSW_BASE/src/Kappa/Skimming/examples/skim_tutorial1_basic.py
+echo
 
+echo "# =================== #"
+echo "# Test python #"
+echo "# =================== #"
 python $CMSSW_BASE/src/Kappa/Skimming/examples/skim_tutorial1_basic.py
 #higgsTauTau/kSkimming_run2_cfg.py
 
 echo "# =================== #"
 echo "# Test cmsRun #"
 echo "# =================== #"
-python $CMSSW_BASE/src/Kappa/Skimming/examples/skim_tutorial1_basic.py
+cmsRun $CMSSW_BASE/src/Kappa/Skimming/examples/skim_tutorial1_basic.py
 #higgsTauTau/kSkimming_run2_cfg.py
 
 echo "# =================== #"
