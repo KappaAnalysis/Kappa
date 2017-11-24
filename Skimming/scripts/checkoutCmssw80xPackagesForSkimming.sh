e283105c1016dcd843391ef65354f2919c4e1f79
Olena Hlushchenko <olena.hlushchenko@physik.rwth-aachen.de>
2017-11-24 13:32:25 +0100

    update the recipe for the tau
    and clean the comments a bit

--- Skimming/scripts/checkoutCmssw80xPackagesForSkimming.sh
+++ Skimming/scripts/checkoutCmssw80xPackagesForSkimming.sh
@@ -22,23 +22,29 @@ do
 done
 
 cd $CMSSW_BASE/src
-#Electron cutBased Id and MVA Id
+
+# EM:
+# Electron cutBased Id and MVA Id
 #https://twiki.cern.ch/twiki/bin/view/CMS/CutBasedElectronIdentificationRun2#Recipe_for_regular_users_for_8_0
 #https://twiki.cern.ch/twiki/bin/view/CMS/MultivariateElectronIdentificationRun2#Recipes_for_regular_users_common
 git cms-addpkg RecoMET/METPUSubtraction
 git cms-addpkg DataFormats/METReco
 git cms-addpkg PhysicsTools/PatUtils
 git cms-addpkg RecoEgamma/ElectronIdentification
-#This needs to be checked out first since there are conflicts with MVA MET otherwise and then 63 packages are checked out...
+#Above needs to be checked out first since there are conflicts with MVA MET otherwise and then 63 packages are checked out...
 git cms-merge-topic -u ikrav:egm_id_80X_v2
-# additional metfilters
+
+# MET:
+# Additional metfilters
 git cms-merge-topic -u cms-met:fromCMSSW_8_0_20_postICHEPfilter
 git cms-merge-topic cms-met:METRecipe_8020 -u
 git cms-merge-topic cms-met:METRecipe_80X_part2 -u
-#packages needed to rerun tau id
-git cms-merge-topic -u cms-tau-pog:CMSSW_8_0_X_tau-pog_miniAOD-backport-tauID
 
-#correct jet corrections for mvamet
+# TAU: Packages needed to rerun tau id
+git cms-merge-topic -u cms-tau-pog:CMSSW_8_0_X_tau-pog_tauIDOnMiniAOD-legacy-backport-81X
+
+# MET:
+# Correct jet corrections for mvamet
 #git cms-merge-topic -u cms-met:METRecipe_8020
 #Mvamet package based on Summer16 Training
 git cms-merge-topic -u macewindu009:mvamet8026
@@ -47,9 +53,12 @@ mkdir $CMSSW_BASE/src/RecoMET/METPUSubtraction/data
 cd $CMSSW_BASE/src/RecoMET/METPUSubtraction/data
 wget https://github.com/macewindu009/MetTools/raw/nicobranch/MVAMET/weightfiles/weightfile.root --no-check-certificate
 
+# CP:
 cd $CMSSW_BASE/src
 git clone https://github.com/artus-analysis/TauRefit.git VertexRefit/TauRefit
-#fetch xml files for Egamma Id from private repository
+
+# EM:
+# fetch xml files for Egamma Id from private repository
 git clone https://github.com/ikrav/RecoEgamma-ElectronIdentification.git tempData/RecoEgamma/ElectronIdentification/data
 cd $CMSSW_BASE/src/tempData/RecoEgamma/ElectronIdentification/data
 git checkout egm_id_80X_v1
@@ -58,7 +67,8 @@ cp -r * $CMSSW_BASE/src
 cd $CMSSW_BASE/src
 rm -rf tempData
 
-#Remove the .git folder as it is not needed and contains a lot of useless data
+# CLEANUP:
+# Remove the .git folder as it is not needed and contains a lot of useless data
 rm -rf RecoEgamma/ElectronIdentification/data/.git
 #Remove Fireworks and SimGeneral - they are only checked out due to failed merge attempts
 rm -rf Fireworks
@@ -72,9 +82,12 @@ git read-tree -mu HEAD
 sed -i "/produces<edm::PtrVector<reco::Muon>>/a \	  produces<bool>();" RecoMET/METFilters/plugins/BadGlobalMuonTagger.cc
 sed -i "/iEvent.put(std::move(out),/a \	iEvent.put(std::auto_ptr<bool>(new bool(found)));" RecoMET/METFilters/plugins/BadGlobalMuonTagger.cc
 sed "/import\ switchJetCollection/a from\ RecoMET\.METProducers\.METSignificanceParams_cfi\ import\ METSignificanceParams_Data" PhysicsTools/PatUtils/python/tools/runMETCorrectionsAndUncertainties.py -i
-#Check out Kappa
+
+# Check out Kappa:
 git clone https://github.com/KappaAnalysis/Kappa.git -b ${KAPPA_BRANCH}
 
+
+# Compile:
 scram b -v -j `grep -c ^processor /proc/cpuinfo` || {
       echo "The ${CMSSW_BASE} with Kappa could not be built"
       exit 1
