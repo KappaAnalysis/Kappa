519f14c51960d774a03a97f53c53069b36dd3d43
Olena Hlushchenko <greyxray@gmail.com>
2018-03-29 17:21:10 +0200

    EventWeightCountProducer: fix the indentation

--- Producers/plugins/EventWeightCountProducer.cc
+++ Producers/plugins/EventWeightCountProducer.cc
@@ -4,18 +4,15 @@
 //
 /**\class EventWeightCountProducer EventWeightCountProducer.cc Kappa/Producers/plugins/EventWeightCountProducer.cc
 
-Description: An event counter that fills a histogram with positive and negative weights in the lumi block
-
+	Description: An event counter that fills a histogram with positive and negative weights in the lumi block
 */
 
 #if (CMSSW_MAJOR_VERSION >= 7)
-// system include files
 #include <memory>
 #include <vector>
 #include <algorithm>
 #include <iostream>
 
-// user include files
 #include "FWCore/Framework/interface/Frameworkfwd.h"
 #include "FWCore/Framework/interface/one/EDProducer.h"
 #include "FWCore/Framework/interface/Event.h"
@@ -26,52 +23,43 @@ Description: An event counter that fills a histogram with positive and negative 
 #include "DataFormats/Common/interface/MergeableCounter.h"
 #include "SimDataFormats/GeneratorProducts/interface/GenEventInfoProduct.h"
 #include "TH1F.h"
-class EventWeightCountProducer : public edm::one::EDProducer<edm::one::WatchLuminosityBlocks,
-																											 edm::EndLuminosityBlockProducer> {
-public:
-	explicit EventWeightCountProducer(const edm::ParameterSet&);
-	~EventWeightCountProducer();
 
-private:
-	virtual void produce(edm::Event &, const edm::EventSetup&) override;
-	virtual void beginLuminosityBlock(const edm::LuminosityBlock &, const edm::EventSetup&) override;
-	virtual void endLuminosityBlock(edm::LuminosityBlock const&, const edm::EventSetup&) override;
-	virtual void endLuminosityBlockProduce(edm::LuminosityBlock &, const edm::EventSetup&) override;
+class EventWeightCountProducer : public edm::one::EDProducer<edm::one::WatchLuminosityBlocks, edm::EndLuminosityBlockProducer>
+{
+	public:
+		explicit EventWeightCountProducer(const edm::ParameterSet&);
+		~EventWeightCountProducer();
 
-	// ----------member data ---------------------------
-	unsigned int nEvents;
-	bool isMC;
-	bool countNegWeightsOnly;
-	edm::EDGetTokenT<GenEventInfoProduct> srcGenEventInfoToken_;
-	edm::InputTag srcGenEventInfo_;
+	private:
+		virtual void produce(edm::Event &, const edm::EventSetup&) override;
+		virtual void beginLuminosityBlock(const edm::LuminosityBlock &, const edm::EventSetup&) override;
+		virtual void endLuminosityBlock(edm::LuminosityBlock const&, const edm::EventSetup&) override;
+		virtual void endLuminosityBlockProduce(edm::LuminosityBlock &, const edm::EventSetup&) override;
+
+		unsigned int nEvents;
+		bool isMC;
+		bool countNegWeightsOnly;
+		edm::EDGetTokenT<GenEventInfoProduct> srcGenEventInfoToken_;
+		edm::InputTag srcGenEventInfo_;
 };
-
-
 
 using namespace edm;
 using namespace std;
 
-
-
-EventWeightCountProducer::EventWeightCountProducer(const edm::ParameterSet& iConfig){
+EventWeightCountProducer::EventWeightCountProducer(const edm::ParameterSet& iConfig)
+{
 	#if CMSSW_MAJOR_VERSION >= 10  || (CMSSW_MAJOR_VERSION == 9 && CMSSW_MINOR_VERSION >= 4 )
 		produces<edm::MergeableCounter, edm::Transition::EndLuminosityBlock>();
 	#else
 	produces<edm::MergeableCounter, edm::InLumi>();
 	#endif
-	if (iConfig.existsAs<bool>("isMC"))
-		isMC = iConfig.getParameter<bool>("isMC");
+	if (iConfig.existsAs<bool>("isMC")) isMC = iConfig.getParameter<bool>("isMC");
 	else throw cms::Exception("Configuration")<<"Missing parameter input isMC \n";\
 
-	if (iConfig.existsAs<bool>("countNegWeightsOnly"))
-		countNegWeightsOnly = iConfig.getParameter<bool>("countNegWeightsOnly");
+	if (iConfig.existsAs<bool>("countNegWeightsOnly")) countNegWeightsOnly = iConfig.getParameter<bool>("countNegWeightsOnly");
 	else throw cms::Exception("Configuration")<<"Missing parameter countNegWeightsOnly \n";
 
-//  if(not isMC and countNegWeightsOnly)
-//  {
-//    throw cms::Exception("Configuration")<<"Configuration error: trying to count negative event weights in data \n";
-//  }
-
+	//  if (not isMC && countNegWeightsOnly) throw cms::Exception("Configuration")<<"Configuration error: trying to count negative event weights in data \n";
 
 	if (isMC)
 	{
@@ -81,37 +69,31 @@ EventWeightCountProducer::EventWeightCountProducer(const edm::ParameterSet& iCon
 	//else throw cms::Exception("Configuration")<<"Missing parameter input srcGenEventInfo \n";
 }
 
-
 EventWeightCountProducer::~EventWeightCountProducer(){}
 
+void EventWeightCountProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
 
-void EventWeightCountProducer::produce(edm::Event& iEvent, const edm::EventSetup& iSetup){
-
-	if(not isMC || not countNegWeightsOnly)
-	{
-				nEvents++;
-	}
+	if (not isMC || not countNegWeightsOnly) nEvents++;
 	else
 	{
 		edm::Handle<GenEventInfoProduct> GenEventInfoHandle;
 		iEvent.getByToken(srcGenEventInfoToken_, GenEventInfoHandle);
-		if( GenEventInfoHandle->weight() < 0)
-		{
-				nEvents++;
-		}
+		if( GenEventInfoHandle->weight() < 0) nEvents++;
 	}
 	return;
 }
 
-
-void EventWeightCountProducer::beginLuminosityBlock(const LuminosityBlock & theLuminosityBlock, const EventSetup & theSetup) {
+void EventWeightCountProducer::beginLuminosityBlock(const LuminosityBlock & theLuminosityBlock, const EventSetup & theSetup)
+{
 	nEvents = 0;
 	return;
 }
 
 void EventWeightCountProducer::endLuminosityBlock(LuminosityBlock const& theLuminosityBlock, const EventSetup & theSetup) {}
 
-void EventWeightCountProducer::endLuminosityBlockProduce(LuminosityBlock & theLuminosityBlock, const EventSetup & theSetup) {
+void EventWeightCountProducer::endLuminosityBlockProduce(LuminosityBlock & theLuminosityBlock, const EventSetup & theSetup)
+{
 	#if CMSSW_MAJOR_VERSION < 8  || (CMSSW_MAJOR_VERSION == 8 && CMSSW_MINOR_VERSION == 0 )
 	auto_ptr<edm::MergeableCounter> numEventsPtr(new edm::MergeableCounter);
 	numEventsPtr->value = nEvents;
@@ -124,9 +106,6 @@ void EventWeightCountProducer::endLuminosityBlockProduce(LuminosityBlock & theLu
 	return;
 }
 
-
-
 //define this as a plug-in
 DEFINE_FWK_MODULE(EventWeightCountProducer);
 #endif
-
